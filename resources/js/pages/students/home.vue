94230c05d099f3244fa9f5ca276fcd30ef6b1959
Eric Kean <kreut@hotmail.com>
2020-06-24 09:01:48 -0700

    Removed anonymous function so that this was properly referenced

--- resources/js/pages/students/home.vue
+++ resources/js/pages/students/home.vue
@@ -13,17 +13,20 @@ export default {
     assignments: ''
   }),
   mounted () {
-    try {
-      //todo: turn this into a function and call that way
-      axios.get('/api/assignments').then(
-        response => this.assignments = response.data
-      );
-    } catch (error) {
-      // todo: force error
-      alert(error.response)
+    this.getAssignments();
+
+  },
+  methods: {
+    getAssignments() {
+      try {
+        axios.get('/api/assignments').then(
+          response => this.assignments = response.data
+        )
+      } catch (error) {
+        alert(error.response)
+      }
     }
   },
-  methods: {},
   metaInfo () {
     return { title: this.$t('home') }
   }
