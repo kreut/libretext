2e7a57d3e60d88c1ccab589ca21ac359cd640463
Eric Kean <kreut@hotmail.com>
2020-06-25 10:55:47 -0700

    Fixed Vue warning involving string instead of array

--- resources/js/pages/students/home.vue
+++ resources/js/pages/students/home.vue
@@ -1,34 +1,34 @@
 <template>
-  <div>
-    <b-table striped hover :items="assignments"></b-table>
-  </div>
+<div>
+  <b-table striped hover :items="assignments"></b-table>
+</div>
 </template>
 
 <script>
-import axios from 'axios'
+  import axios from 'axios'
 
-export default {
-  middleware: 'auth',
-  data: () => ({
-    assignments: ''
-  }),
-  mounted () {
-    this.getAssignments();
+  export default {
+    middleware: 'auth',
+    data: () => ({
+      assignments: []
+    }),
+    mounted () {
+      this.getAssignments();
 
-  },
-  methods: {
-    getAssignments() {
-      try {
-        axios.get('/api/assignments').then(
-          response => this.assignments = response.data
-        )
-      } catch (error) {
-        alert(error.response)
+    },
+    methods: {
+      getAssignments() {
+        try {
+          axios.get('/api/assignments').then(
+            response => this.assignments = response.data
+          )
+        } catch (error) {
+          alert(error.response)
+        }
       }
+    },
+    metaInfo () {
+      return { title: this.$t('home') }
     }
-  },
-  metaInfo () {
-    return { title: this.$t('home') }
   }
-}
 </script>
