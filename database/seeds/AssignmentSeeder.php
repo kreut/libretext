fde0306d5f3e8b7f5e661258602c0797880d5344
Eric Kean <kreut@hotmail.com>
2020-07-07 16:07:03 -0700

    New seeder for the assignments

--- database/seeds/AssignmentSeeder.php
+++ database/seeds/AssignmentSeeder.php
@@ -18,12 +18,14 @@ class AssignmentSeeder extends Seeder
         $course = Course::find(1);
         $current_date = Carbon::now();
 
-        for($i=0; $i<=10; $i++):
+        for ($i = 0; $i <= 10; $i++):
             $current_date = $current_date->add('1 week');
             Assignment::create([
                 'name' => $faker->text(15),
-                'available_on' => $current_date->add(($i+2) . ' weeks')->format('Y-m-d H:i:s'),
-                'due_date' => $current_date->add(($i+3) . ' weeks')->format('Y-m-d H:i:s'),
+                'available_on' => $current_date->add(($i + 2) . ' weeks')->format('Y-m-d H:i:00'),
+                'due_date' => $current_date->add(($i + 3) . ' weeks')->format('Y-m-d H:i:00'),
+                'num_submissions_needed' => $faker->randomElement([2, 3, 4, 5, 6, 7, 8, 9, 10]),
+                'type_of_submission' => $faker->randomElement(['completed', 'correct']),
                 'course_id' => $course->id
             ]);
         endfor;
