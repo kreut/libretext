805cb3dcd162e68b59605cdd356fe848a71dad3c
Eric Kean <kreut@hotmail.com>
2020-07-15 13:06:15 -0700

    Working on better navigation

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,4 +1,56 @@
 <template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">
+      <router-link :to="{ name: user ? 'home' : 'welcome' }" class="navbar-brand">
+      {{ appName }}
+    </router-link>
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav >
+      <b-navbar-nav v-if="user">
+        <b-nav-item href="#">
+          <router-link :to="{ name: 'courses.index' }" class="nav-link" active-class="active">
+            Courses
+          </router-link>
+        </b-nav-item>
+
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto" >
+
+        <b-nav-item-dropdown right v-if="user">
+          <!-- Using 'button-content' slot -->
+          <template  v-slot:button-content>
+            <em>Hi, {{ user.first_name }}!</em>
+          </template>
+          <router-link :to="{ name: 'settings.profile' }" class="dropdown-item pl-3">
+            <fa icon="cog" fixed-width />
+            {{ $t('settings') }}
+          </router-link>
+         <a href="#" class="dropdown-item pl-3" @click.prevent="logout">
+            <fa icon="sign-out-alt" fixed-width />
+            {{ $t('logout') }}
+          </a>
+        </b-nav-item-dropdown>
+        <b-navbar-nav v-if="!user">
+          <b-nav-item href="#">
+            <router-link :to="{ name: 'login' }" class="nav-link" active-class="active">
+            {{ $t('login') }}
+          </router-link>
+          </b-nav-item>
+            <b-nav-item href="#">
+              <router-link :to="{ name: 'register' }" class="nav-link" active-class="active">
+                {{ $t('register') }}
+              </router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  <!--
   <nav class="navbar navbar-expand-lg navbar-light bg-white">
     <div class="container">
       <router-link :to="{ name: user ? 'home' : 'welcome' }" class="navbar-brand">
@@ -12,13 +64,13 @@
       <div id="navbarToggler" class="collapse navbar-collapse">
         <ul class="navbar-nav">
           <locale-dropdown />
-          <!-- <li class="nav-item">
+           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
-          </li> -->
+          </li>
         </ul>
 
         <ul class="navbar-nav ml-auto">
-          <!-- Authenticated -->
+
           <li v-if="user" class="nav-item dropdown">
             <a class="nav-link dropdown-toggle text-dark"
                href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
@@ -39,7 +91,6 @@
               </a>
             </div>
           </li>
-          <!-- Guest -->
           <template v-else>
             <li class="nav-item">
               <router-link :to="{ name: 'login' }" class="nav-link" active-class="active">
@@ -55,7 +106,7 @@
         </ul>
       </div>
     </div>
-  </nav>
+  </nav>-->
 </template>
 
 <script>
