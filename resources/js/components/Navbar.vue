40b06515c83c604bfd58b66191386b9b43aeb092
Eric Kean <kreut@hotmail.com>
2020-07-20 12:32:55 -0700

    Specific registration dropdown

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -2,16 +2,16 @@
   <b-navbar toggleable="lg" type="dark" variant="info">
     <b-navbar-brand href="#">
       <router-link :to="{ name: user ? 'home' : 'welcome' }" class="navbar-brand">
-      {{ appName }}
-    </router-link>
+        {{ appName }}
+      </router-link>
     </b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav >
+    <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav v-if="user">
         <b-nav-item href="#" v-if="user">
-          <router-link v-if="$route.name !== 'courses.index'" :to="{ name: 'courses.index' }" class="nav-link" >
+          <router-link v-if="$route.name !== 'courses.index'" :to="{ name: 'courses.index' }" class="nav-link">
             My Courses
           </router-link>
         </b-nav-item>
@@ -19,33 +19,43 @@
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto" >
+      <b-navbar-nav class="ml-auto">
 
         <b-nav-item-dropdown right v-if="user">
           <!-- Using 'button-content' slot -->
-          <template  v-slot:button-content>
+          <template v-slot:button-content>
             <em>Hi, {{ user.first_name }}!</em>
           </template>
           <router-link :to="{ name: 'settings.profile' }" class="dropdown-item pl-3">
-            <fa icon="cog" fixed-width />
+            <fa icon="cog" fixed-width/>
             {{ $t('settings') }}
           </router-link>
-         <a href="#" class="dropdown-item pl-3" @click.prevent="logout">
-            <fa icon="sign-out-alt" fixed-width />
+          <a href="#" class="dropdown-item pl-3" @click.prevent="logout">
+            <fa icon="sign-out-alt" fixed-width/>
             {{ $t('logout') }}
           </a>
         </b-nav-item-dropdown>
         <b-navbar-nav v-if="!user">
-          <b-nav-item href="#">
+
+          <b-navbar-nav>
+          <b-nav-item href="/login">
             <router-link :to="{ name: 'login' }" class="nav-link" active-class="active">
-            {{ $t('login') }}
-          </router-link>
+              {{ $t('login') }}
+            </router-link>
           </b-nav-item>
-            <b-nav-item href="#">
-              <router-link :to="{ name: 'register' }" class="nav-link" active-class="active">
-                {{ $t('register') }}
-              </router-link>
-          </b-nav-item>
+          </b-navbar-nav>
+
+          <b-nav-item-dropdown text="Register" right>
+            <b-dropdown-item href="#"> <router-link  :to="{ path: '/register/student' }" class="dropdown-item pl-3">
+             Student
+            </router-link>
+            </b-dropdown-item>
+            <b-dropdown-item href="#"> <router-link  :to="{ path: '/register/instructor' }" class="dropdown-item pl-3">
+              Instructor
+            </router-link>
+            </b-dropdown-item>
+          </b-nav-item-dropdown>
+
         </b-navbar-nav>
       </b-navbar-nav>
     </b-collapse>
@@ -53,38 +63,36 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import LocaleDropdown from './LocaleDropdown'
+  import {mapGetters} from 'vuex'
+  import LocaleDropdown from './LocaleDropdown'
 
-export default {
-  components: {
-    LocaleDropdown
-  },
+  export default {
+    components: {
+      LocaleDropdown
+    },
 
-  data: () => ({
-    appName: window.config.appName
-  }),
+    data: () => ({
+      appName: window.config.appName
+    }),
 
-  computed: mapGetters({
-    user: 'auth/user'
-  }),
+    computed: mapGetters({
+      user: 'auth/user'
+    }),
 
-  methods: {
-    async logout () {
-      // Log out the user.
-      await this.$store.dispatch('auth/logout')
+    methods: {
+      async logout() {
+        // Log out the user.
+        await this.$store.dispatch('auth/logout')
 
-      // Redirect to login.
-      this.$router.push({ name: 'login' })
+        // Redirect to login.
+        this.$router.push({name: 'login'})
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-.profile-photo {
-  width: 2rem;
-  height: 2rem;
-  margin: -.375rem 0;
-}
+  .nav-link {
+    padding-top: .25em;
+  }
 </style>
