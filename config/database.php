b94d2e6ba23fea890c3a0f5af526dc35250c4ace
Eric Kean <kreut@hotmail.com>
2020-07-22 12:15:38 -0700

    Importing data from webwork

--- config/database.php
+++ config/database.php
@@ -60,6 +60,24 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        'webwork' => [
+            'driver' => 'mysql',
+            'host' => env('DB_WEBWORK_HOST'),
+            'port' => env('DB_WEBWORK_PORT', '3306'),
+            'database' => env('DB_WEBWORK_DATABASE', 'forge'),
+            'username' => env('DB_WEBWORK_USERNAME', 'forge'),
+            'password' => env('DB_WEBWORK_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
