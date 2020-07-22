13e39ce8b5809b1bbc5cff193e686954f32378e3
Eric Kean <kreut@hotmail.com>
2020-07-22 06:19:45 -0700

    Solved the Date problem and now use webpack.mix to mix other js files

--- resources/js/helpers/Date.js
+++ resources/js/helpers/Date.js
@@ -1,5 +1,5 @@
 export const formatDate  = value =>  {
   let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-  let date = new Date(value)
+  let date = new Date(value.replace(' ', 'T'))
   return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -8,6 +8,11 @@ mix
   .js('resources/js/app.js', 'public/dist/js')
   .sass('resources/sass/app.scss', 'public/dist/css')
 
+mix.babel([
+  'resources/js/helpers/Date.js',
+  'resources/js/helpers/LoginRedirect.js'
+], 'public/assets/js/combined.js')
+
   .disableNotifications()
 
 if (mix.inProduction()) {
