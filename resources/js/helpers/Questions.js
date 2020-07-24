c14f7e03174806408d825ee37be76a935dff1800
Eric Kean <kreut@hotmail.com>
2020-07-24 09:46:28 -0700

    H5P questions can now come from live or dev server

--- resources/js/helpers/Questions.js
+++ resources/js/helpers/Questions.js
@@ -2,7 +2,8 @@ export function getSrc(question){
   let src
   switch (question.technology){
     case 'h5p':
-      src = `https://h5p.libretexts.org/wp-admin/admin-ajax.php?action=h5p_embed&id=${question.technology_id}`
+      let url = (window.location.host.includes('adapt.libretexts.org') && !window.location.host.includes('dev')) ? 'h5p.libretexts.org' : 'dev.h5p.libretexts.org'
+      src = `https://${url}/wp-admin/admin-ajax.php?action=h5p_embed&id=${question.technology_id}`
       break;
     case 'webwork':
       src = `https://webwork.libretexts.org/webwork2/html2xml?answersSubmitted=0&sourceFilePath=Library/${question.technology_id}&problemSeed=1234567&courseID=anonymous&userID=anonymous&course_password=anonymous&showSummary=1&displayMode=MathJax&language=en&outputformat=libretexts`
