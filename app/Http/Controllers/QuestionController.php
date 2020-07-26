50e2f5928666d2558470f48e3d84da1feb997716
Eric Kean <kreut@hotmail.com>
2020-07-26 05:49:31 -0700

    Implemented multiple tags for searching

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -12,8 +12,23 @@ class QuestionController extends Controller
     public function getQuestionsByTags(Request $request)
     {
         //get all questions with these tags
-        $tag = Tag::where(['tag' => $request->get('tags')])->first();
-        if (!$tag) return ['type' => 'error'];
+        $chosen_tags = Tag::whereIn('tag',  $request->get('tags'));
+
+        if (!$chosen_tags) return ['type' => 'error'];
+        $tag_ids = [];
+        foreach ($chosen_tags as $chosen_tag){
+            array_push($tag_ids, $chosen_tag->id);
+
+        }
+        Start here!!!  Then change below
+        SELECT question_id
+FROM question_tag
+WHERE tag_id IN (438,3066)
+GROUP BY question_id
+HAVING COUNT(*) = 2
+
+
+
         foreach ($tag->questions as $key => $question){
             $tag->questions[$key]['inAssignment'] = false;
         }
