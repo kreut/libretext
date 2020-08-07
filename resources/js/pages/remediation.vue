a4858c062eb22ed96055b23dffe3200912f93bdc
Eric Kean <kreut@hotmail.com>
2020-08-07 07:05:41 -0700

    Removed the groups

--- resources/js/pages/remediation.vue
+++ resources/js/pages/remediation.vue
@@ -7,11 +7,6 @@
         <span id="add" class="btn-link pl-2" v-on:click="addRemediation">Add</span>
         <span class="view btn-link pl-2">View</span>
       </div>
-      <div id="subnav">
-        <div id="group-1" class="navactive side">Group 1</div>
-        <div id="group-2" class="navdisabled side">Group 2</div>
-        <div id="group-3" class="navdisabled side">Group 3</div>
-      </div>
       <div id="blocklist">
         <div class="blockelem create-flowy noselect">
           <input type="hidden" name='blockelemtype' class="blockelemtype" value="1">
@@ -147,20 +142,6 @@
         drag.innerHTML += "<div class='blockyleft'><img src='assets/timeblue.svg'><p class='blockyname'>Time has passed</p></div><div class='blockyright'><img src='assets/img/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When <span>10 seconds</span> have passed</div>";
       } else if (drag.querySelector(".blockelemtype").value == "4") {
         drag.innerHTML += "<div class='blockyleft'><img src='assets/errorblue.svg'><p class='blockyname'>Error prompt</p></div><div class='blockyright'><img src='assets/img/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When <span>Error 1</span> is triggered</div>";
-      } else if (drag.querySelector(".blockelemtype").value == "5") {
-        drag.innerHTML += "<div class='blockyleft'><img src='assets/databaseorange.svg'><p class='blockyname'>New database entry</p></div><div class='blockyright'><img src='assets/img/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Add <span>Data object</span> to <span>Database 1</span></div>";
-      } else if (drag.querySelector(".blockelemtype").value == "6") {
-        drag.innerHTML += "<div class='blockyleft'><img src='assets/databaseorange.svg'><p class='blockyname'>Update database</p></div><div class='blockyright'><img src='assets/img/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Update <span>Database 1</span></div>";
-      } else if (drag.querySelector(".blockelemtype").value == "7") {
-        drag.innerHTML += "<div class='blockyleft'><img src='assets/actionorange.svg'><p class='blockyname'>Perform an action</p></div><div class='blockyright'><img src='assets/img/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Perform <span>Action 1</span></div>";
-      } else if (drag.querySelector(".blockelemtype").value == "8") {
-        drag.innerHTML += "<div class='blockyleft'><img src='assets/twitterorange.svg'><p class='blockyname'>Make a tweet</p></div><div class='blockyright'><img src='assets/img/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Tweet <span>Query 1</span> with the account <span>@alyssaxuu</span></div>";
-      } else if (drag.querySelector(".blockelemtype").value == "9") {
-        drag.innerHTML += "<div class='blockyleft'><img src='assets/logred.svg'><p class='blockyname'>Add new log entry</p></div><div class='blockyright'><img src='assets/img/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Add new <span>success</span> log entry</div>";
-      } else if (drag.querySelector(".blockelemtype").value == "10") {
-        drag.innerHTML += "<div class='blockyleft'><img src='assets/logred.svg'><p class='blockyname'>Update logs</p></div><div class='blockyright'><img src='assets/img/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Edit <span>Log Entry 1</span></div>";
-      } else if (drag.querySelector(".blockelemtype").value == "11") {
-        drag.innerHTML += "<div class='blockyleft'><img src='assets/errorred.svg'><p class='blockyname'>Prompt an error</p></div><div class='blockyright'><img src='assets/img/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Trigger <span>Error 1</span></div>";
       }
       return true;
     }
@@ -171,20 +152,8 @@
     function release() {
       tempblock2.classList.remove("blockdisabled");
     }
-    var disabledClick = function(){
-      document.querySelector(".navactive").classList.add("navdisabled");
-      document.querySelector(".navactive").classList.remove("navactive");
-      this.classList.add("navactive");
-      this.classList.remove("navdisabled");
-      if (this.getAttribute("id") == "group-1") {
-        document.getElementById("blocklist").innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="1"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/img/eye.svg"></div><div class="blocktext">                        <p class="blocktitle">New visitor</p><p class="blockdesc">Triggers when somebody visits a specified page</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="2"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Action is performed</p><p class="blockdesc">Triggers when somebody performs a specified action</p></div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="3"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/time.svg"></div><div class="blocktext">                        <p class="blocktitle">Time has passed</p><p class="blockdesc">Triggers after a specified amount of time</p>          </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="4"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Error prompt</p><p class="blockdesc">Triggers when a specified error happens</p>              </div></div></div>';
-      } else if (this.getAttribute("id") == "group-2") {
-        document.getElementById("blocklist").innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="5"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/database.svg"></div><div class="blocktext">                        <p class="blocktitle">New database entry</p><p class="blockdesc">Adds a new entry to a specified database</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="6"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/database.svg"></div><div class="blocktext">                        <p class="blocktitle">Update database</p><p class="blockdesc">Edits and deletes database entries and properties</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="7"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Perform an action</p><p class="blockdesc">Performs or edits a specified action</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="8"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/twitter.svg"></div><div class="blocktext">                        <p class="blocktitle">Make a tweet</p><p class="blockdesc">Makes a tweet with a specified query</p>        </div></div></div>';
-      } else if (this.getAttribute("id") == "group-3") {
-        document.getElementById("blocklist").innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="9"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/log.svg"></div><div class="blocktext">                        <p class="blocktitle">Add new log entry</p><p class="blockdesc">Adds a new log entry to this project</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="10"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/log.svg"></div><div class="blocktext">                        <p class="blocktitle">Update logs</p><p class="blockdesc">Edits and deletes log entries in this project</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="11"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Prompt an error</p><p class="blockdesc">Triggers a specified error</p>        </div></div></div>';
-      }
-    }
-    addEventListenerMulti("click", disabledClick, false, ".side");
+
+
     document.getElementById("close").addEventListener("click", function(){
       if (rightcard) {
         rightcard = false;
@@ -621,51 +590,6 @@ body, html {
 .navactive {
   color: #393C44!important;
 }
-#group-1 {
-  margin-left: 20px;
-  font-family: Roboto;
-  font-weight: 500;
-  font-size: 14px;
-  text-align: center;
-  color: #808292;
-  width: calc(88% / 3);
-  height: 48px;
-  line-height: 48px;
-  display: inline-block;
-  float: left;
-}
-.navactive:after {
-  display: block;
-  content: "";
-  width:  100%;
-  height: 4px;
-  background-color: #217CE8;
-  margin-top: -4px;
-}
-#group-2 {
-  display: inline-block;
-  font-family: Roboto;
-  font-weight: 500;
-  color: #808292;
-  font-size: 14px;
-  height: 48px;
-  line-height: 48px;
-  width: calc(88% / 3);
-  text-align: center;
-  float: left;
-}
-#group-3 {
-  width: calc(88% / 3);
-  display: inline-block;
-  font-family: Roboto;
-  font-weight: 500;
-  color: #808292;
-  font-size: 14px;
-  height: 48px;
-  line-height: 48px;
-  text-align: center;
-}
-
 
 .blockelem:first-child {
   margin-top: 20px
