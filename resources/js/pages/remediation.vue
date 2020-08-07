eff8ab7c0b7a1f5211b45c1b7a4c171b80f3599e
Eric Kean <kreut@hotmail.com>
2020-08-07 05:47:15 -0700

    Removed header

--- resources/js/pages/remediation.vue
+++ resources/js/pages/remediation.vue
@@ -1,24 +1,5 @@
 <template>
   <div>
-    <div id="navigation">
-      <div id="leftside">
-        <div id="details">
-          <div id="back"><img src="assets/arrow.svg"></div>
-          <div id="names">
-            <p id="title">Your automation pipeline</p>
-            <p id="subtitle">Marketing automation</p>
-          </div>
-        </div>
-      </div>
-      <div id="centerswitch">
-        <div id="leftswitch">Diagram view</div>
-        <div id="rightswitch">Code editor</div>
-      </div>
-      <div id="buttonsright">
-        <div id="discard">Discard</div>
-        <div id="publish">Publish to site</div>
-      </div>
-    </div>
     <div id="leftcard">
       <div id="closecard">
         <img src="assets/closeleft.svg">
