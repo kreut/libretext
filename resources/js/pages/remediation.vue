f85a21f4d87454cfaa436e469f85527350f00b9a
Eric Kean <kreut@hotmail.com>
2020-08-07 06:48:41 -0700

    Changed to Group 1, Group 2, Group 3

--- resources/js/pages/remediation.vue
+++ resources/js/pages/remediation.vue
@@ -3,14 +3,14 @@
     <div id="leftcard">
       <div id="search">
         <img src="assets/img/search.svg">
-        <input type="text" placeholder="Query Id">
+        <input v-model="chosenId" type="text" placeholder="Query Id">
         <span id="add" class="btn-link pl-2" v-on:click="addRemediation">Add</span>
         <span class="view btn-link pl-2">View</span>
       </div>
       <div id="subnav">
-        <div id="triggers" class="navactive side">Triggers</div>
-        <div id="actions" class="navdisabled side">Actions</div>
-        <div id="loggers" class="navdisabled side">Loggers</div>
+        <div id="group-1" class="navactive side">Group 1</div>
+        <div id="group-2" class="navdisabled side">Group 2</div>
+        <div id="group-3" class="navdisabled side">Group 3</div>
       </div>
       <div id="blocklist">
         <div class="blockelem create-flowy noselect">
@@ -78,13 +78,6 @@
           </div>
         </div>
       </div>
-      <div id="footer">
-        <a href="https://github.com/alyssaxuu/flowy/" target="_blank">GitHub</a>
-        <span>&#183;</span>
-        <a href="https://twitter.com/alyssaxuu/status/1199724989353730048" target="_blank">Twitter</a>
-        <span>&#183;</span>
-        <a href="https://alyssax.com" target="_blank"><p>Made with</p><img src="assets/heart.svg"><p>by</p> Alyssa X</a>
-      </div>
     </div>
     <div id="propwrap">
       <div id="properties">
@@ -124,7 +117,8 @@
     },
 
     data: () => ({
-      title: window.config.appName
+      title: window.config.appName,
+      chosenId: ''
     }),
 
   mounted() {
@@ -182,11 +176,11 @@
       document.querySelector(".navactive").classList.remove("navactive");
       this.classList.add("navactive");
       this.classList.remove("navdisabled");
-      if (this.getAttribute("id") == "triggers") {
+      if (this.getAttribute("id") == "group-1") {
         document.getElementById("blocklist").innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="1"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/img/eye.svg"></div><div class="blocktext">                        <p class="blocktitle">New visitor</p><p class="blockdesc">Triggers when somebody visits a specified page</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="2"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Action is performed</p><p class="blockdesc">Triggers when somebody performs a specified action</p></div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="3"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/time.svg"></div><div class="blocktext">                        <p class="blocktitle">Time has passed</p><p class="blockdesc">Triggers after a specified amount of time</p>          </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="4"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Error prompt</p><p class="blockdesc">Triggers when a specified error happens</p>              </div></div></div>';
-      } else if (this.getAttribute("id") == "actions") {
+      } else if (this.getAttribute("id") == "group-2") {
         document.getElementById("blocklist").innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="5"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/database.svg"></div><div class="blocktext">                        <p class="blocktitle">New database entry</p><p class="blockdesc">Adds a new entry to a specified database</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="6"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/database.svg"></div><div class="blocktext">                        <p class="blocktitle">Update database</p><p class="blockdesc">Edits and deletes database entries and properties</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="7"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Perform an action</p><p class="blockdesc">Performs or edits a specified action</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="8"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/twitter.svg"></div><div class="blocktext">                        <p class="blocktitle">Make a tweet</p><p class="blockdesc">Makes a tweet with a specified query</p>        </div></div></div>';
-      } else if (this.getAttribute("id") == "loggers") {
+      } else if (this.getAttribute("id") == "group-3") {
         document.getElementById("blocklist").innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="9"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/log.svg"></div><div class="blocktext">                        <p class="blocktitle">Add new log entry</p><p class="blockdesc">Adds a new log entry to this project</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="10"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/log.svg"></div><div class="blocktext">                        <p class="blocktitle">Update logs</p><p class="blockdesc">Edits and deletes log entries in this project</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="11"><div class="grabme"><img src="assets/img/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Prompt an error</p><p class="blockdesc">Triggers a specified error</p>        </div></div></div>';
       }
     }
@@ -237,7 +231,7 @@
     },
     methods: {
       addRemediation() {
-        alert('f')
+        alert(this.chosenId)
       }
     }
   }
@@ -627,7 +621,7 @@ body, html {
 .navactive {
   color: #393C44!important;
 }
-#triggers {
+#group-1 {
   margin-left: 20px;
   font-family: Roboto;
   font-weight: 500;
@@ -648,7 +642,7 @@ body, html {
   background-color: #217CE8;
   margin-top: -4px;
 }
-#actions {
+#group-2 {
   display: inline-block;
   font-family: Roboto;
   font-weight: 500;
@@ -660,7 +654,7 @@ body, html {
   text-align: center;
   float: left;
 }
-#loggers {
+#group-3 {
   width: calc(88% / 3);
   display: inline-block;
   font-family: Roboto;
