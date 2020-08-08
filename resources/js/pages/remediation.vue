3cb35ca6eacde9adebfce8df93e11dee014a345a
Eric Kean <kreut@hotmail.com>
2020-08-08 05:40:41 -0700

    Changed the select message

--- resources/js/pages/remediation.vue
+++ resources/js/pages/remediation.vue
@@ -74,11 +74,9 @@ export default {
     chosenId: '',
     selected: null,
     options: [
-      { value: null, text: 'Please select an option' },
-      { value: 'a', text: 'This is First option' },
-      { value: 'b', text: 'Selected Option' },
-      { value: { C: '3PO' }, text: 'This is an option with object value' },
-      { value: 'd', text: 'This one is disabled', disabled: true }
+      { value: null, text: 'Please select  the library' },
+      { value: 'query', text: 'Query' },
+      { value: 'b', text: 'Some other library' },
     ]
   }),
 
