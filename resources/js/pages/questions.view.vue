b698e0305e2653065cf831d6a25a4c9f8dd36e73
Eric Kean <kreut@hotmail.com>
2020-08-12 15:48:13 -0700

    Can filter out remediation by parent

--- resources/js/pages/questions.view.vue
+++ resources/js/pages/questions.view.vue
@@ -1,42 +1,45 @@
 <template>
   <div>
 
-      <div v-if="!initializing">
-        <PageTitle v-bind:title="this.title"></PageTitle>
-        <div v-if="questions.length">
-          <div class="d-flex justify-content-between">
-            <div class="mt-1 mb-2" v-on:click="getQuestionsForAssignment()" v-if="user.role !== 3">
-              <b-button variant="success">Get Questions</b-button>
-            </div>
-            <div class="overflow-auto">
-              <b-pagination
-                v-model="currentPage"
-                :total-rows="questions.length"
-                :per-page="perPage"
-                first-number
-                last-number
-              ></b-pagination>
-            </div>
-            <div class="mt-1 mb-2" v-on:click="removeQuestion(currentPage)" v-if="user.role !== 3">
-              <b-button variant="danger">Remove Question</b-button>
-            </div>
+    <div v-if="!initializing">
+      <PageTitle v-bind:title="this.title"></PageTitle>
+      <div v-if="questions.length">
+        <li v-for="remediation in currentLearningTreeLevel">{{ remediation }}</li>
+        <div class="d-flex justify-content-between">
+          <div class="mt-1 mb-2" v-on:click="getQuestionsForAssignment()" v-if="user.role !== 3">
+            <b-button variant="success">Get Questions</b-button>
           </div>
-          <iframe allowtransparency="true" frameborder="0"
-                  v-bind:src="questions[currentPage-1].src"
-                  style="overflow: auto; height: 1274px;"
-                  width="100%">
-
-          </iframe>
-
+          <div class="overflow-auto">
+            <b-pagination
+              v-model="currentPage"
+              :total-rows="questions.length"
+              :per-page="perPage"
+              first-number
+              last-number
+              v-on:input="resetLearningTree(questions[currentPage - 1].learning_tree)"
+            ></b-pagination>
+          </div>
+          <div class="mt-1 mb-2" v-on:click="removeQuestion(currentPage)" v-if="user.role !== 3">
+            <b-button variant="danger">Remove Question</b-button>
+          </div>
         </div>
-        <div v-else>
-          <div v-if="questions !== ['init']">
-            <div class="mt-1 mb-2" v-on:click="getQuestionsForAssignment()" v-if="user.role !== 3">
-              <b-button variant="success">Get More Questions</b-button>
-            </div>
-            <div class="mt-4">
-              <b-alert :show="true" variant="warning"><a href="#" class="alert-link">This assignment currently has no questions.
-              </a></b-alert>
+        <iframe allowtransparency="true" frameborder="0"
+                v-bind:src="questions[currentPage-1].src"
+                style="overflow: auto; height: 1274px;"
+                width="100%">
+
+        </iframe>
+
+      </div>
+      <div v-else>
+        <div v-if="questions !== ['init']">
+          <div class="mt-1 mb-2" v-on:click="getQuestionsForAssignment()" v-if="user.role !== 3">
+            <b-button variant="success">Get More Questions</b-button>
+          </div>
+          <div class="mt-4">
+            <b-alert :show="true" variant="warning"><a href="#" class="alert-link">This assignment currently has no
+              questions.
+            </a></b-alert>
           </div>
         </div>
       </div>
@@ -46,102 +49,125 @@
 
 
 <script>
-  import axios from 'axios'
-  import {mapGetters} from "vuex"
-  import { getSrc } from '~/helpers/Questions'
+import axios from 'axios'
+import {mapGetters} from "vuex"
+import {getSrc} from '~/helpers/Questions'
 
-  export default {
+export default {
 
-    middleware: 'auth',
-    computed: mapGetters({
-      user: 'auth/user'
-    }),
-    data: () => ({
-      perPage: 1,
-      currentPage: 1,
-      questions: [],
-      initializing: true, //use to show a blank screen until all is loaded
-      title: '',
-      assignmentId: ''
-    }),
-    created() {
-      this.getSrc = getSrc
-    },
-    mounted() {
-      this.assignmentId = this.$route.params.assignmentId
-      this.getTitle(this.assignmentId)
-      this.getSelectedQuestions(this.assignmentId)
-      let vm = this
-      if (this.user.role === 3) {
-        let receiveMessage = async function (event) {
-          if (event.data.action !== 'hello') {
-            let submission_data = {
-              'submission': event.data,
-              'assignment_id': vm.assignmentId,
-              'question_id': vm.questions[vm.currentPage - 1].id
-            }
-            console.log(submission_data)
-
-            //if incorrect, show the learning tree stuff...
-            let {data} = await axios.post('/api/submissions', submission_data)
-            console.log(data)
-            if (data.message){
-              //Will add this later when we've worked out what it means to submit...vm.$noty[data.type](data.message)
-            }
-
-
-          } else {
-            console.log ('Hello Event')
+  middleware: 'auth',
+  computed: mapGetters({
+    user: 'auth/user'
+  }),
+  data: () => ({
+    level: 0,
+    learningTree: [],
+    currentLearningTreeLevel: [],
+    parentId: 0,
+    perPage: 1,
+    currentPage: 1,
+    questions: [],
+    initializing: true, //use to show a blank screen until all is loaded
+    title: '',
+    assignmentId: ''
+  }),
+  created() {
+    this.getSrc = getSrc
+  },
+  mounted() {
+    this.assignmentId = this.$route.params.assignmentId
+    this.getTitle(this.assignmentId)
+    this.getSelectedQuestions(this.assignmentId)
+    let vm = this
+    if (this.user.role === 3) {
+      let receiveMessage = async function (event) {
+        if (event.data.action !== 'hello') {
+          let submission_data = {
+            'submission': event.data,
+            'assignment_id': vm.assignmentId,
+            'question_id': vm.questions[vm.currentPage - 1].id
           }
-        }
-        window.addEventListener("message", receiveMessage, false)
-      }
-    },
-    methods: {
-      async getTitle(assignmentId) {
-        try {
-          const {data} = await axios.get(`/api/assignments/${assignmentId}`)
-          this.title = `${data.name} Assignment Questions`
-        } catch (error) {
-          this.title = "View Questions"
-        }
-      },
-      async getSelectedQuestions(assignmentId) {
-        try {
-          const {data} = await axios.get(`/api/assignments/${assignmentId}/questions/view`)
-          console.log(JSON.parse(JSON.stringify(data)))
-//create an array of arrays?
+          console.log(submission_data)
 
-          if (data.type === 'error'){
-            this.$noty.error(data.message)
-            return false
-          }
-          this.questions = data.questions
-          for (let i = 0; i < this.questions.length; i++) {
-            this.questions[i].src = this.getSrc(this.questions[i])
+          //if incorrect, show the learning tree stuff...
+          let {data} = await axios.post('/api/submissions', submission_data)
+          console.log(data)
+          if (data.message) {
+            //Will add this later when we've worked out what it means to submit...vm.$noty[data.type](data.message)
           }
 
-          this.initializing = false
-        } catch (error) {
-          alert(error)
-          this.$noty.error('We could not retrieve the questions for this assignment.  Please try again or contact us for assistance.')
-        }
-      },
-      getQuestionsForAssignment() {
-        this.$router.push(`/assignments/${this.assignmentId}/questions/get`)
-      },
-      async removeQuestion(currentPage) {
-        try {
-          axios.delete(`/api/assignments/${this.assignmentId}/questions/${this.questions[currentPage - 1].id}`)
-          this.$noty.info('The question has been removed from the assignment.')
-          this.questions.splice(currentPage - 1, 1);
-        } catch (error) {
-          this.$noty.error('We could not remove the question from the assignment.  Please try again or contact us for assistance.')
+        } else {
+          console.log('Hello Event')
         }
       }
-    },
-    metaInfo() {
-      return {title: this.$t('home')}
+      window.addEventListener("message", receiveMessage, false)
     }
+  },
+  methods: {
+    resetLearningTree(learningTree) {
+      console.log(learningTree)
+      if (learningTree) {
+        this.level = 0
+        this.parentId = 0
+        this.learningTree = learningTree
+        //loop through and get all with parent = -1
+        this.currentLearningTreeLevel = []
+alert(this.learningTree.length)
+        //loop through each with parent having this level
+        for (let i = 0; i < this.learningTree.length; i++) {
+          console.log(this.learningTree[i].parent)
+          if (this.learningTree[i].parent === 0) {
+            alert('f')
+            this.currentLearningTreeLevel.push(learningTree[i])
+          }
+        }
+        console.info(this.currentLearningTreeLevel)
+      }
+    },
+    async getTitle(assignmentId) {
+      try {
+        const {data} = await axios.get(`/api/assignments/${assignmentId}`)
+        this.title = `${data.name} Assignment Questions`
+      } catch (error) {
+        this.title = "View Questions"
+      }
+    },
+    async getSelectedQuestions(assignmentId) {
+      try {
+        const {data} = await axios.get(`/api/assignments/${assignmentId}/questions/view`)
+        console.log(JSON.parse(JSON.stringify(data)))
+
+
+        if (data.type === 'error') {
+          this.$noty.error(data.message)
+          return false
+        }
+        this.questions = data.questions
+        for (let i = 0; i < this.questions.length; i++) {
+          this.questions[i].src = this.getSrc(this.questions[i])
+        }
+
+        this.initializing = false
+      } catch (error) {
+        alert(error)
+        this.$noty.error('We could not retrieve the questions for this assignment.  Please try again or contact us for assistance.')
+      }
+    },
+    getQuestionsForAssignment() {
+      this.$router.push(`/assignments/${this.assignmentId}/questions/get`)
+    },
+    async removeQuestion(currentPage) {
+      try {
+        axios.delete(`/api/assignments/${this.assignmentId}/questions/${this.questions[currentPage - 1].id}`)
+        this.$noty.info('The question has been removed from the assignment.')
+        this.questions.splice(currentPage - 1, 1);
+      } catch (error) {
+        this.$noty.error('We could not remove the question from the assignment.  Please try again or contact us for assistance.')
+      }
+    }
+  },
+  metaInfo() {
+    return {title: this.$t('home')}
   }
+}
 </script>
