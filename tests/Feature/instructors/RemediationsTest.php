7bd126be65803b66a1a03d4c6299b72fe7c1f773
Eric Kean <kreut@hotmail.com>
2020-08-16 17:59:36 -0700

    Started RemediationsTest

---
+++ tests/Feature/instructors/RemediationsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RemediationsTest extends TestCase
+{
+  public function can_get_learning_tree_if_owner(){
+
+
+  }
+
+    public function cannot_get_learning_tree_if_owner(){
+
+
+    }
+
+    public function can_save_learning_tree_if_owner(){
+
+
+    }
+    public function cannot_save_learning_tree_if_not_owner(){
+
+
+    }
+
+    public function can_get_student_learning_objectives(){
+
+
+    }
+}
