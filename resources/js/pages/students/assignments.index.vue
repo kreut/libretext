85bf42176c08d2d153614a6a36dbc528067073e2
Eric Kean <kreut@hotmail.com>
2020-08-17 20:17:05 -0700

    Can get an assignment file in the form

--- resources/js/pages/students/assignments.index.vue
+++ resources/js/pages/students/assignments.index.vue
@@ -2,25 +2,46 @@
   <div>
     <PageTitle v-if="canViewAssignments" title="Assignments"></PageTitle>
     <div v-if="hasAssignments">
+      <b-modal
+        id="modal-upload-assignment-file"
+        ref="modal"
+        title="Upload File"
+        @ok="submitUploadAssignmentFile"
+        @hidden="resetModalForms"
+        ok-title="Submit"
+
+      >
+        <b-form ref="form" @submit="submitUploadAssignmentFile">
+          <b-form-file
+            v-model="form.file"
+            :state="Boolean(form.file)"
+            placeholder="Choose a file or drop it here..."
+            drop-placeholder="Drop file here..."
+          ></b-form-file>
+
+        </b-form>
+      </b-modal>
+
       <b-table striped hover :fields="fields" :items="assignments">
         <template v-slot:cell(name)="data">
           <div class="mb-0">
             <a href="" v-on:click.prevent="getStudentView(data.item.id)">{{ data.item.name }}</a>
           </div>
         </template>
-          <template v-slot:cell(uploadFile)="data">
-            <div class="mb-0">
-              <button class="btn btn-primary" v-on:click="openUploadAssignmentFileModal(data.item.id)">Upload File</button>
-            </div>
-          </template>
+        <template v-slot:cell(files)="data">
+          <div class="mb-0">
+            <b-button variant="primary" v-on:click="openUploadAssignmentFileModal(data.item.id)" v-b-modal.modal-upload-assignment-file>Upload File</b-button>
+          </div>
+        </template>
 
       </b-table>
     </div>
     <div v-else>
       <br>
       <div class="mt-4">
-      <b-alert :show="showNoAssignmentsAlert" variant="warning"><a href="#" class="alert-link">This course currently has
-        no assignments.</a></b-alert>
+        <b-alert :show="showNoAssignmentsAlert" variant="warning"><a href="#" class="alert-link">This course currently
+          has
+          no assignments.</a></b-alert>
       </div>
     </div>
   </div>
@@ -28,22 +49,22 @@
 
 <script>
   import axios from 'axios'
-
-
-
+  import Form from "vform"
   const now = new Date()
-
 
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
   let formatDateAndTime = value => {
     let date = new Date(value)
-    return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear() +  ' ' + date.toLocaleTimeString()
+    return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear() + ' ' + date.toLocaleTimeString()
   }
 
 
   export default {
     middleware: 'auth',
     data: () => ({
+      form: new Form({
+        file: null
+      }),
       assignments: [],
       courseId: false,
       fields: [
@@ -61,10 +82,7 @@
           }
         },
         'credit_given_if_at_least',
-        {
-          key: 'uploadFile',
-          name: 'Files'
-        }
+       'files'
       ],
       hasAssignments: false,
       showNoAssignmentsAlert: false,
@@ -75,20 +93,26 @@
       this.getAssignments()
     },
     methods: {
+      submitUploadAssignmentFile() {
+       console.log(this.form.file)
+      },
+      resetModalForms() {
+        alert('reset modal')
+      },
       openUploadAssignmentFileModal(assignmentId) {
-alert(assignmentId)
+        this.assignmentId = assignmentId
       },
       getStudentView(assignmentId) {
         this.$router.push(`/assignments/${assignmentId}/questions/view`)
       },
       async getAssignments() {
         try {
-         const {data}  = await axios.get(`/api/assignments/courses/${this.courseId}`)
+          const {data} = await axios.get(`/api/assignments/courses/${this.courseId}`)
           console.log(data)
-        if (data.type === 'error') {
-          this.$noty.error(data.message)
-          return false
-        }
+          if (data.type === 'error') {
+            this.$noty.error(data.message)
+            return false
+          }
           this.canViewAssignments = true
           this.hasAssignments = data.length > 0
           this.showNoAssignmentsAlert = !this.hasAssignments
