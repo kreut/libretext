a547efd2ad47a88bae6d22ed9a9ef7f3a91ff886
Eric Kean <kreut@hotmail.com>
2020-09-01 13:14:28 -0700

    Added an index to the page_id in questions

---
+++ database/migrations/2020_09_01_201235_update_page_id_to_questions.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePageIdToQuestions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->unsignedBigInteger('page_id')->change();
+            $table->unique('page_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+    }
+}
