e5a5af154d7c353e9feacffe04c57e46320911d4
Eric Kean <kreut@hotmail.com>
2020-09-05 16:45:02 -0700

    Event driven update of tags and location

--- app/Http/Controllers/MindTouchEventController.php
+++ app/Http/Controllers/MindTouchEventController.php
@@ -2,14 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Question;
+use App\Query;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\LOG;
+
+use App\Exceptions\Handler;
+use \Exception;
 
 class MindTouchEventController extends Controller
 {
-    public function update(Request $request) {
-        Log:info('made it!');
-        Log::info( $request->page_id );
+    public function update(Request $request)
+    {
+        LOG::info($request->all());
 
+
+        /*  Could be:
+         * 1.  A new Question
+         * 2.  Updated Tags
+         * 3.  A change in the location name
+         */
+        //new tags
+        $tags = $request->tags;
+        if ($tags) {
+            $tags = json_decode($request->tags, true);
+            $lower_case_tags = [];
+            foreach ($tags as $key => $tag) {
+                $lower_case_tags[$key] = mb_strtolower($tag);
+            }
+        }
+
+
+        $Query = new Query();
+        try {
+            DB::beginTransaction();
+            $question = Question::where('page_id', $request->page_id)->first();
+            if ($question) {
+                LOG::info('question exists');
+                LOG::info('Current Location: ' . $question->location);
+                //maybe the location was updated....
+                if ($question->location !== $request->location) {
+                    $question->location = $request->location;
+                    $question->save();
+                }
+            } else {
+                LOg::info('new question');
+                //new question
+                $question = Question::create(['page_id' => $request->page_id,
+                    'technology' => 'h5p',
+                    'location' => $request->location]);
+            }
+            //handle the tags
+            if ($tags) {
+                LOG::info('updating tags');
+                $question->tags()->detach();//get rid of the current tags
+                $Query->addTagsToQuestion($question, $lower_case_tags);
+            } else {
+                Log::info('not updating tags');
+            }
+            DB::commit();
+
+        } catch (Exception $e) {
+            DB::rollback();
+            $h = new Handler(app());
+            $h->report($e);
+        }
     }
 }
--- resources/js/helpers/Mindtouch.js
+++ resources/js/helpers/Mindtouch.js
@@ -1,26 +1,47 @@
 function updateInAdapt() {
-  var iframe = document.createElement("iframe");
-  var uniqueString = "MyUNiQueId";
+  let iframe = document.createElement("iframe");
+  let uniqueString = "MyUNiQueId";
   document.body.appendChild(iframe);
   iframe.style.display = "none";
   iframe.contentWindow.name = uniqueString;
 
 // construct a form with hidden inputs, targeting the iframe
-  var form = document.createElement("form");
+  let form = document.createElement("form");
   form.target = uniqueString;
   form.action = "https://dev.adapt.libretexts.org/api/mind-touch-events/update";
   form.method = "POST";
 
 // repeat for each parameter
-  var input = document.createElement("input");
-  input.type = "hidden";
-  input.name = "page_id";
-  var mt_global_settings = $('#mt-global-settings');
-  var data = JSON.parse($('#mt-global-settings').html());
+  let input1 = document.createElement("input");
+  input1.type = "hidden";
+  input1.name = "page_id";
+
+  let data = JSON.parse($('#mt-global-settings').html());
+  input1.value = data.pageId;
 
 
-  input.value = data.pageId;
-  form.appendChild(input);
+  let input2 = document.createElement("input");
+  input2.type = "hidden";
+  input2.name = "location";
+  input2.value = $('#mt-summary').attr('data-page-uri');
+
+    let input3 = document.createElement("input");
+    input3.type = "hidden";
+    input3.name = "tags";
+    let tags = {};
+    if ($('.live-tag-entry').length) {
+      $('.live-tag-entry').each(function (i) {
+        tags[i] = $(this).find('a').html()
+      })
+    }
+    input3.value = JSON.stringify(tags)
+    console.log(tags);
+
+  form.appendChild(input1);
+  form.appendChild(input2);
+
+  form.appendChild(input3);
+
 
   document.body.appendChild(form);
   form.submit();
@@ -28,18 +49,33 @@ function updateInAdapt() {
 
 }
 
+$(document).ready(function(){
+  updateInAdapt();///for a name change
+});
+window.tagObserverSet = false;
+//More Details https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+$(document).on('click', '#mt-summary', function() {
+  if (!window.tagObserverSet) {
+    let target = document.querySelector('#live-tag-entries')
+    let observer = new MutationObserver(function (mutations) {
+      console.log('tags changed');
+      updateInAdapt();
+    });
 
+    let config = {attributes: true, childList: true, characterData: true};
+    observer.observe(target, config);
+    window.tagObserverSet = true;
+    console.log('created tag observer');
+  }
+});
+
+//handle changes in the editor
+//Don't actually need this but I'm going to save it just in case.
 $(document).on('click', '#page-options-menu-edit', function () {
   CKEDITOR.on('instanceReady', function () {
     console.log('ready');
-    var ck_18 = $('a[class*="mindtouchsave"]').first();
-    var onclick = ck_18.attr('onclick');
-    ck_18.attr('onclick', 'updateInAdapt();' + onclick);
-
-
+    let save = $('a[class*="mindtouchsave"]').first();
+    let onclick = save.attr('onclick');
+    save.attr('onclick', 'updateInAdapt();' + onclick);
   });
 });
-
-
-
-
