3227dc7beaf74e70e51f77125902ec40dc6a54ec
Eric Kean <kreut@hotmail.com>
2020-09-17 07:36:50 -0700

    Tests written for the profile

--- tests/Feature/SettingsTest.php
+++ tests/Feature/SettingsTest.php
@@ -18,7 +18,51 @@ class SettingsTest extends TestCase
         $this->user = factory(User::class)->create();
     }
 
+    /** @test */
 
+    public function first_name_must_be_valid(){
+        $this->actingAs($this->user)
+            ->patchJson('/api/settings/profile', [
+                'first_name' => '',
+                'last_name' => 'User',
+                'email' => 'test@test.app',
+            ])
+            ->assertJsonValidationErrors(['first_name']);
+    }
+/** @test */
+    public function last_name_must_be_valid(){
+        $this->actingAs($this->user)
+            ->patchJson('/api/settings/profile', [
+                'first_name' => 'some name',
+                'last_name' => '',
+                'email' => 'test@test.app',
+            ])
+            ->assertJsonValidationErrors(['last_name']);
+    }
+
+    /** @test */
+    public function time_zone_must_be_valid(){
+        $this->actingAs($this->user)
+            ->patchJson('/api/settings/profile', [
+                'first_name' => 'some name',
+                'last_name' => 'last name',
+                'email' => 'test@test.app',
+                'time_zone' => 'bad time zone'
+            ])
+            ->assertJsonValidationErrors(['time_zone']);
+    }
+
+    /** @test */
+    public function email_must_be_valid(){
+        $this->actingAs($this->user)
+            ->patchJson('/api/settings/profile', [
+                'first_name' => 'some name',
+                'last_name' => 'last name',
+                'email' => 'bad email',
+            ])
+            ->assertJsonValidationErrors(['email']);
+    }
+/** @test */
     public function update_profile_info()
     {
         $this->actingAs($this->user)
@@ -26,19 +70,21 @@ class SettingsTest extends TestCase
                 'first_name' => 'Test',
                 'last_name' => 'User',
                 'email' => 'test@test.app',
+                'time_zone' => 'America/New_York'
             ])
             ->assertSuccessful()
-            ->assertJsonStructure(['id', 'name', 'email']);
+            ->assertJson(['type' =>'success']);
 
         $this->assertDatabaseHas('users', [
             'id' => $this->user->id,
             'first_name' => 'Test',
             'last_name' => 'User',
             'email' => 'test@test.app',
+            'time_zone' => 'America/New_York'
         ]);
     }
 
-
+/** @test */
     public function update_password()
     {
         $this->actingAs($this->user)
