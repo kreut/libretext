91143905829e7926f350b177cc44c349b68d1b92
Eric Kean <kreut@hotmail.com>
2020-09-21 06:30:56 -0700

    Can get the unencrypted paylod

--- app/Http/Controllers/JWE2Controller.php
+++ app/Http/Controllers/JWE2Controller.php
@@ -10,8 +10,13 @@ use Jose\Component\Core\JWK;
 use Jose\Component\Encryption\Compression\Deflate;
 use Jose\Component\Encryption\JWEBuilder;
 use Jose\Component\Encryption\Serializer\CompactSerializer;
+use Jose\Component\Encryption\Serializer\JWESerializerManager;
+
+
+use Jose\Component\Encryption\JWEDecrypter;
 
 use Jose\Easy\Load;
+
 class JWE2Controller extends Controller
 {
     public function encode()
@@ -37,6 +42,8 @@ class JWE2Controller extends Controller
             $contentEncryptionAlgorithmManager,
             $compressionMethodManager
         );
+
+        //Our key
         $jwk = new JWK([
             'kty' => 'oct',
             'k' => 'dzI6nbW4OcNF-AtfxGAmuyz7IpHRudBI0WgGjZWgaRJt6prBn3DARXgUR8NVwKhfL43QBIU2Un3AvCGCHRgY4TbEqhOi8-i98xxmCggNjde4oaW6wkJ2NgM3Ss9SOX9zS3lcVzdCMdum-RwVJ301kbin4UtGztuzJBeg5oVN00MGxjC2xWwyI0tgXVs-zJs5WlafCuGfX1HrVkIf5bvpE0MQCSjdJpSeVao6-RSTYDajZf7T88a2eVjeW31mMAg-jzAWfUrii61T_bYPJFOXW8kkRWoa1InLRdG6bKB9wQs9-VdXZP60Q4Yuj_WZ-lO7qV9AEFrUkkjpaDgZT86w2g',
@@ -50,7 +57,7 @@ class JWE2Controller extends Controller
             'iss' => 'My service',
             'aud' => 'Your application',
         ]);
-echo $payload;
+
         $jwe = $jweBuilder
             ->create()              // We want to create a new JWE
             ->withPayload($payload) // We set the payload
@@ -62,38 +69,55 @@ echo $payload;
             ->addRecipient($jwk)    // We add a recipient (a shared key or public key).
             ->build();              // We build it
 
-
         $serializer = new CompactSerializer(); // The serializer
 
         $token = $serializer->serialize($jwe, 0); // We serialize the recipient at index 0 (we only have one recipient).
-echo $token;
-        $jwk = new JWK([
-            'kty' => 'RSA',
-            'kid' => 'bilbo.baggins@hobbiton.example',
-            'use' => 'sig',
-            'n' => 'n4EPtAOCc9AlkeQHPzHStgAbgs7bTZLwUBZdR8_KuKPEHLd4rHVTeT-O-XV2jRojdNhxJWTDvNd7nqQ0VEiZQHz_AJmSCpMaJMRBSFKrKb2wqVwGU_NsYOYL-QtiWN2lbzcEe6XC0dApr5ydQLrHqkHHig3RBordaZ6Aj-oBHqFEHYpPe7Tpe-OfVfHd1E6cS6M1FZcD1NNLYD5lFHpPI9bTwJlsde3uhGqC0ZCuEHg8lhzwOHrtIQbS0FVbb9k3-tVTU4fg_3L_vniUFAKwuCLqKnS2BYwdq_mzSnbLY7h_qixoR7jig3__kRhuaxwUkRz5iaiQkqgc5gHdrNP5zw',
-            'e' => 'AQAB',
-            'd' => 'bWUC9B-EFRIo8kpGfh0ZuyGPvMNKvYWNtB_ikiH9k20eT-O1q_I78eiZkpXxXQ0UTEs2LsNRS-8uJbvQ-A1irkwMSMkK1J3XTGgdrhCku9gRldY7sNA_AKZGh-Q661_42rINLRCe8W-nZ34ui_qOfkLnK9QWDDqpaIsA-bMwWWSDFu2MUBYwkHTMEzLYGqOe04noqeq1hExBTHBOBdkMXiuFhUq1BU6l-DqEiWxqg82sXt2h-LMnT3046AOYJoRioz75tSUQfGCshWTBnP5uDjd18kKhyv07lhfSJdrPdM5Plyl21hsFf4L_mHCuoFau7gdsPfHPxxjVOcOpBrQzwQ',
-            'p' => '3Slxg_DwTXJcb6095RoXygQCAZ5RnAvZlno1yhHtnUex_fp7AZ_9nRaO7HX_-SFfGQeutao2TDjDAWU4Vupk8rw9JR0AzZ0N2fvuIAmr_WCsmGpeNqQnev1T7IyEsnh8UMt-n5CafhkikzhEsrmndH6LxOrvRJlsPp6Zv8bUq0k',
-            'q' => 'uKE2dh-cTf6ERF4k4e_jy78GfPYUIaUyoSSJuBzp3Cubk3OCqs6grT8bR_cu0Dm1MZwWmtdqDyI95HrUeq3MP15vMMON8lHTeZu2lmKvwqW7anV5UzhM1iZ7z4yMkuUwFWoBvyY898EXvRD-hdqRxHlSqAZ192zB3pVFJ0s7pFc',
-            'dp' => 'B8PVvXkvJrj2L-GYQ7v3y9r6Kw5g9SahXBwsWUzp19TVlgI-YV85q1NIb1rxQtD-IsXXR3-TanevuRPRt5OBOdiMGQp8pbt26gljYfKU_E9xn-RULHz0-ed9E9gXLKD4VGngpz-PfQ_q29pk5xWHoJp009Qf1HvChixRX59ehik',
-            'dq' => 'CLDmDGduhylc9o7r84rEUVn7pzQ6PF83Y-iBZx5NT-TpnOZKF1pErAMVeKzFEl41DlHHqqBLSM0W1sOFbwTxYWZDm6sI6og5iTbwQGIC3gnJKbi_7k_vJgGHwHxgPaX2PnvP-zyEkDERuf-ry4c_Z11Cq9AqC2yeL6kdKT1cYF8',
-            'qi' => '3PiqvXQN0zwMeE-sBvZgi289XP9XCQF3VWqPzMKnIgQp7_Tugo6-NZBKCQsMf3HaEGBjTVJs_jcK8-TRXvaKe-7ZMaQj8VfBdYkssbu0NKDDhjJ-GtiseaDVWt7dcH0cfwxgFUHpQh7FoCrjFJ6h6ZEpMF6xmujs4qMpPz8aaI4',
-        ]);
-        $jwt = Load::jwe($token) // We want to load and decrypt the token in the variable $token
-        ->algs(['RSA-OAEP-256', 'RSA-OAEP']) // The key encryption algorithms allowed to be used
-        ->encs(['A256GCM']) // The content encryption algorithms allowed to be used
-        ->exp()
-            ->iat()
-            ->nbf()
-            ->aud('audience1')
-            ->iss('issuer')
-            ->sub('subject')
-            ->jti('0123456789')
-            ->key($jwk) // Key used to decrypt the token
-            ->run() // Go!
-        ;
+//dd($token);
+echo "Encrypted token: $token\r\n\r\n";
 
+// The key encryption algorithm manager with the A256KW algorithm.
+        $keyEncryptionAlgorithmManager = new AlgorithmManager([
+            new A256KW(),
+        ]);
+
+// The content encryption algorithm manager with the A256CBC-HS256 algorithm.
+        $contentEncryptionAlgorithmManager = new AlgorithmManager([
+            new A256CBCHS512(),
+        ]);
+
+// The compression method manager with the DEF (Deflate) method.
+        $compressionMethodManager = new CompressionMethodManager([
+            new Deflate(),
+        ]);
+
+// We instantiate our JWE Decrypter.
+        $jweDecrypter = new JWEDecrypter(
+            $keyEncryptionAlgorithmManager,
+            $contentEncryptionAlgorithmManager,
+            $compressionMethodManager
+        );
+
+        // Our key.
+        $jwk = new JWK([
+            'kty' => 'oct',
+            'k' => 'dzI6nbW4OcNF-AtfxGAmuyz7IpHRudBI0WgGjZWgaRJt6prBn3DARXgUR8NVwKhfL43QBIU2Un3AvCGCHRgY4TbEqhOi8-i98xxmCggNjde4oaW6wkJ2NgM3Ss9SOX9zS3lcVzdCMdum-RwVJ301kbin4UtGztuzJBeg5oVN00MGxjC2xWwyI0tgXVs-zJs5WlafCuGfX1HrVkIf5bvpE0MQCSjdJpSeVao6-RSTYDajZf7T88a2eVjeW31mMAg-jzAWfUrii61T_bYPJFOXW8kkRWoa1InLRdG6bKB9wQs9-VdXZP60Q4Yuj_WZ-lO7qV9AEFrUkkjpaDgZT86w2g',
+        ]);
+
+// The serializer manager. We only use the JWE Compact Serialization Mode.
+        $serializerManager = new JWESerializerManager([
+            new CompactSerializer(),
+        ]);
+
+// The input we want to decrypt
+        //  $token = 'eyJhbGciOiJBMjU2S1ciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiemlwIjoiREVGIn0.9RLpf3Gauf05QPNCMzPcH4XNBLmH0s3e-YWwOe57MTG844gnc-g2ywfXt_R0Q9qsR6WhkmQEhdLk2CBvfqr4ob4jFlvJK0yW.CCvfoTKO9tQlzCvbAuFAJg.PxrDlsbSRcxC5SuEJ84i9E9_R3tCyDQsEPTIllSCVxVcHiPOC2EdDlvUwYvznirYP6KMTdKMgLqxB4BwI3CWtys0fceSNxrEIu_uv1WhzJg.4DnyeLEAfB4I8Eq0UobnP8ymlX1UIfSSADaJCXr3RlU';
+
+// We try to load the token.
+        $jwe = $serializerManager->unserialize($token);
+
+// We decrypt the token. This method does NOT check the header.
+        $success = $jweDecrypter->decryptUsingKey($jwe, $jwk, 0);
+
+        echo "\r\n" . $jwe->getPayload();
 
     }
 
