967fea56332c1e69f4b88ffc9776a8e0c388f372
Eric Kean <kreut@hotmail.com>
2020-10-15 10:29:29 -0700

    The jwt stuff works!!!!!

--- app/Http/Controllers/JWTController.php
+++ app/Http/Controllers/JWTController.php
@@ -21,15 +21,15 @@ class JWTController extends Controller
         $JWE = new JWE();
         $token = $JWE->encrypt('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYwMjU5NTM5MSwiZXhwIjoyNDY2NTk1MzkxLCJuYmYiOjE2MDI1OTUzOTEsImp0aSI6IlV5alhWSlRzdHdmbkNyZjEiLCJzdWIiOjEsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.-o0_89Kc5dqt58pbFGw4AktqrndDPb_L5lEmRY4Vqes');
         echo "The encrypted token: " . $token;
-        echo "The decrypted token: " . $JWE->decrypt('eyJwMnMiOiJoSVprUURjRGhsaHhHcGZoSlE0eFpUQ3Y0UER5bmJwN3pRMUxJaXB4d1BIdEktX0NDSzhyeTFlMGJRbEQxa0NkeS1ZUy1qcndXTzc5V3JFSXhFTWJyZyIsInAyYyI6NDA5NiwiYWxnIjoiUEJFUzItSFM1MTIrQTI1NktXIiwiZW5jIjoiQTI1NkdDTSIsInppcCI6IkRFRiJ9.MI8w-3sW6H6nkY_IGIj_PJkbPp_tP4Vz222XukJrrm1HGD5HH-nYTg.hl2NGPjGbYqE6B9n.0vD1ps-m9megozlEgnAQ1GPOBnVe34xHqOELoZqAGi33bKKB2eujpmDcxR1d9jLA3wSabN2YygXWC_03nEzRAyu3T3Oe3_5pM4TOf7s.fSk977KyZQrLMDYV_Bbxhg');
+        echo "The decrypted token: " . $JWE->decrypt('eyJwMnMiOiJBb3lIZDFBQlE3XzRNcGdPSVFKTFg1QlhuSVlBOTA2SUNTalZBQUpLTExfbFp5SG1KdEpmQndIMjdTX2FBcHhlQ1pjYzFwUkJKREljaEg2R2FHUURUUSIsInAyYyI6NDA5NiwiYWxnIjoiUEJFUzItSFM1MTIrQTI1NktXIiwiZW5jIjoiQTI1NkdDTSIsInppcCI6IkRFRiJ9.udv_AQDWDbx4H2ff8wNJ2fpp17ac_2f2HSHBKUfVoEAA-ffm6Iaqyw.812SfE7su1llwNpt.wvvxDXnBa-HF8Vkp6yWdL4dOAk8oGifShIDhinlQzG4txL2zZVhT2cLRZ9_tIPggmyK2KYCEKLBYmGqnpunQRpEhxd0OqIMrRigOG16OzlnfmX4rJyd_Dabvuwri5UYMGCsPjeCKPi_llUqQc_O8hrbdCxucNxXpcP3r-AK5j30BxChscnw239XuFchHtiAaiRf0ZqT_AGhaGzUJmCIlGT0pZTFW-aRRR10dz3tfzWcQy3siVTrkDuFcd8VeLuObp_szHvXHldkkckySPrwiWz4jiQa8B-2082akQInvB35flouPmJRyZ4vzDbKToP6yEQHhcaIDzbha0Yx12ptVmcRf2PrBhboYAOT-P3hMIafPfQsd12ucTYqva28Lfs7BMq31CeDr-r8YEOjeryDDekXDdPJnRm3eMNLDuGlMRLkj2VQ4s3fiU1UL320ofMjMK6mMJd5pQY8Tl9um6A89p_ABlPxk_vLE4qr1J0K9lG4DsELf862tppp2t0lrj6-RWHsITOEEOU-0_lkvUR7RrYuvo0tKHLanWJD6sXJmg83ECuGAZKJxcnukiTr3Xr-d_xK3lGLtb-F5g6RHSv_Rh4PlTFIxLs6fgSqjQECHLvfg.mUCksybpo09RlxRMoJFQog');
     }
 
-    public function validateToken($request)
+    public function validateToken(string $content)
     {
         //Webwork should post the answerJWT with Authorization using the Adapt JWT
         $response['type'] = 'error';
         try {
-            if (!$user = auth()->setRequest($request)->user()) {
+            if (!auth()->setToken($content)->getPayload()) {
                 $response['message'] = 'User not found';
             } else {
                 $response['type'] = 'success';
@@ -47,12 +47,12 @@ class JWTController extends Controller
 
         $content = $request->getContent();
         Log::info($content);
-        $response = $this->validateToken($request);
+        $response = $this->validateToken($content);
         if ($response['type'] === 'error') {
             return json_encode($response);
         }
 
-
+        Log::info(auth()->user()->id);
         $answerJWT = $this->getPayload($content);
 //if the token isn't formed correctly return a message
         if (!isset($answerJWT->problemJWT)) {
