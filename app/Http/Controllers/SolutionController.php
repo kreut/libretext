dbf8311025bdc8331b077a6b8203d19ed216f40a
Eric Kean <kreut@hotmail.com>
2020-10-17 13:39:38 -0700

    Can send the assignment solution to S3

--- app/Http/Controllers/SolutionController.php
+++ app/Http/Controllers/SolutionController.php
@@ -24,14 +24,7 @@ class SolutionController extends Controller
     public function storeSolutionFile(Request $request, Solution $Solution)
     {
 
-/**
-    If it's a cutup...
-    1. Save the solution file as a whole file with assignment as the row (need this for students later)
-    2. Do the cutup and add those to the cutup database
-    3. Allow them to do the cutups
 
-    4. If a student, do the cut up right off the bat and save to the cutup database then let them add.
-**/
         $response['type'] = 'error';
 
         try {
@@ -51,26 +44,61 @@ class SolutionController extends Controller
                 $response['message'] = $validator->errors()->first('solutionFile');
                 return $response;
             }
-        $question_id = $request->questionId;
-        $user_id = Auth::user()->id;
-        $file = $request->file("solutionFile")->store("solutions/$user_id", 'local');
+            $assignment_id = $request->assignmentId;
+            $question_id = $request->questionId;
+            $user_id = Auth::user()->id;
 
-        $solutionContents = Storage::disk('local')->get($file);
+            switch ($request->uploadLevel) {
 
-        Storage::disk('s3')->put($file, $solutionContents, ['StorageClass' => 'STANDARD_IA']);
-        $original_filename = $request->file("solutionFile")->getClientOriginalName();
-        $file_data = [
-            'file' => basename($file),
-            'original_filename' => $original_filename,
-            'updated_at' => Carbon::now()];
-        $Solution->updateOrCreate(
-            ['user_id' => $user_id,
-                'question_id' => $question_id],
-            $file_data
-        );
-        $response['type'] = 'success';
-        $response['message'] = 'Your solution has been saved.';
-        $response['original_filename'] = $original_filename;
+                case('question'):
+                    $file = $request->file("solutionFile")->store("solutions/$user_id", 'local');
+                    $solutionContents = Storage::disk('local')->get($file);
+                    Storage::disk('s3')->put($file, $solutionContents, ['StorageClass' => 'STANDARD_IA']);
+                    $original_filename = $request->file("solutionFile")->getClientOriginalName();
+                    $file_data = [
+                        'file' => basename($file),
+                        'original_filename' => $original_filename,
+                        'updated_at' => Carbon::now()];
+                    $Solution->updateOrCreate(
+                        ['user_id' => $user_id,
+                            'question_id' => $question_id],
+                        $file_data
+                    );
+                    $response['type'] = 'success';
+                    $response['message'] = 'Your solution has been saved.';
+                    $response['original_filename'] = $original_filename;
+                    break;
+                case('assignment'):
+                    $file = $request->file("solutionFile")->store("solutions/$user_id", 'local');
+                    $assignment_file =  "solutions/$user_id/assignment-$assignment_id.pdf";
+                    Storage::move($file, $assignment_file);
+                    $solutionContents = Storage::disk('local')->get($assignment_file);
+                    Storage::disk('s3')->put($assignment_file, $solutionContents, ['StorageClass' => 'STANDARD_IA']);
+
+
+                    dd($assignment_file);
+
+
+                    $original_filename = $request->file("solutionFile")->getClientOriginalName();
+                    $file_data = [
+                        'file' => basename($file),
+                        'original_filename' => $original_filename,
+                        'updated_at' => Carbon::now()];
+                    $Solution->updateOrCreate(
+                        ['user_id' => $user_id,
+                            'question_id' => $question_id],
+                        $file_data
+                    );
+                    $response['type'] = 'success';
+                    $response['message'] = 'Your solution has been saved.';
+                    $response['original_filename'] = $original_filename;
+                    break;
+                default:
+                    $response['message'] = 'That is not a valid upload level.  Please contact us for assistance.';
+
+
+            }
+
         } catch (Exception $e) {
             $h = new Handler(app());
             $h->report($e);
@@ -96,8 +124,8 @@ class SolutionController extends Controller
         $file_creator_user_id = $assignment->course->user_id;
         try {
             $solution_file = $solution->where('user_id', $file_creator_user_id)
-            ->where('question_id', $request->question_id)
-            ->first()->file;
+                ->where('question_id', $request->question_id)
+                ->first()->file;
 
             return Storage::disk('s3')->download("solutions/$file_creator_user_id/$solution_file");
         } catch (Exception $e) {
