ed99d7d7fa66d003ccbd3eb2eff16c157328c7e4
Eric Kean <kreut@hotmail.com>
2020-11-03 06:14:56 -0800

    Refactored the scores

---
+++ resources/js/helpers/Scores.js
@@ -0,0 +1,56 @@
+import axios from 'axios'
+
+let stats = require("stats-lite")
+
+export async function getScoresSummary(id, url) {
+  const {data} = await axios.get(url)
+  console.log(data)
+  this.scores = data.scores.map(user => parseFloat(user.score))
+  console.log(this.scores)
+  this.max = Math.max(...this.scores) //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+  this.min = Math.min(...this.scores)
+  this.mean = Math.round(stats.mean(this.scores) * 100) / 100
+  this.stdev = Math.round(stats.stdev(this.scores) * 100) / 100
+  this.range = this.max - this.min
+  let precision
+  if (this.max < 20) {
+    precision = 1
+  } else if (this.max < 50) {
+    precision = 5
+  } else {
+    precision = 10
+  }
+
+  let labels = []
+  let counts = []
+  for (let i = 0; i < this.scores.length; i++) {
+    let score = this.round(this.scores[i], precision)
+    if (!labels.includes(score)) {
+      labels.push(score)
+      counts.push(0)
+    }
+  }
+  console.log(counts)
+
+  labels = labels.sort((a, b) => a - b)
+  console.log(labels)
+  for (let i = 0; i < this.scores.length; i++) {
+    for (let j = 0; j < labels.length; j++) {
+      let score = this.round(this.scores[i], precision)
+      if (parseFloat(score) === parseFloat(labels[j])) {
+        counts[j]++
+        break
+      }
+    }
+  }
+
+  return {
+    labels: labels,
+    datasets: [
+      {
+        backgroundColor: 'green',
+        data: counts
+      }
+    ]
+  }
+}
--- resources/js/pages/assignments.view.vue
+++ resources/js/pages/assignments.view.vue
@@ -24,6 +24,7 @@
               <scores v-if="loaded"
                       :chartdata="chartdata"
                       :height="300"
+                      ref="scores"
               />
             </b-col>
           </b-row>
@@ -36,10 +37,9 @@
 
 
 import {mapGetters} from "vuex"
-import axios from 'axios'
 import Scores from '~/components/Scores'
+import {getScoresSummary} from '~/helpers/Scores'
 
-let stats = require("stats-lite")
 export default {
   components: {Scores},
   middleware: 'auth',
@@ -61,9 +61,11 @@ export default {
 
   async mounted() {
     this.loaded = false
+    this.getScoresSummary = getScoresSummary
     this.assignmentId = this.$route.params.assignmentId
     try {
-      const scoresData = await this.getAssignmentSummary(this.assignmentId)
+
+      const scoresData = await this.getScoresSummary(this.assignmentId,`/api/assignments/${this.assignmentId}`)
       console.log(scoresData)
       this.chartdata = scoresData
       this.loaded = true
@@ -79,60 +81,6 @@ export default {
       num = parseFloat(num)
       if (!precision) return num
       return (Math.round(num / precision) * precision)
-    },
-    async getAssignmentSummary(assignmentId) {
-      const {data} = await axios.get(`/api/assignments/${assignmentId}`)
-      console.log(data)
-      this.assignmentInfo = data
-
-      this.scores = this.assignmentInfo.scores.map(user => parseFloat(user.score))
-      console.log(this.scores)
-      this.max = Math.max(...this.scores) //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
-      this.min = Math.min(...this.scores)
-      this.mean = Math.round(stats.mean(this.scores) * 100) / 100
-      this.stdev = Math.round(stats.stdev(this.scores) * 100) / 100
-      this.range = this.max - this.min
-      let precision
-      if (this.max < 20) {
-        precision = 1
-      } else if (this.max < 50) {
-        precision = 5
-      } else {
-        precision = 10
-      }
-
-      let labels = []
-      let counts = []
-      for (let i = 0; i < this.scores.length; i++) {
-        let score = this.round(this.scores[i], precision)
-        if (!labels.includes(score)) {
-          labels.push(score)
-          counts.push(0)
-        }
-      }
-      console.log(counts)
-
-      labels = labels.sort((a, b) => a - b)
-      console.log(labels)
-      for (let i = 0; i < this.scores.length; i++) {
-        for (let j = 0; j < labels.length; j++) {
-          let score = this.round(this.scores[i], precision)
-          if (parseFloat(score) === parseFloat(labels[j])) {
-            counts[j]++
-            break
-          }
-        }
-      }
-
-      return {
-        labels: labels,
-        datasets: [
-          {
-            backgroundColor: 'green',
-            data: counts
-          }
-        ]
-      }
     },
     metaInfo() {
       return {title: this.$t('home')}
