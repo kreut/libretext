8ea4e656f4a056945a4b474ed46b3db3b436aa09
Eric Kean <kreut@hotmail.com>
2020-11-16 10:04:34 -0800

    Added an InstructorAccessCode factory

--- database/factories/InstructorAccessCodeFactory.php
+++ database/factories/InstructorAccessCodeFactory.php
@@ -2,11 +2,17 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\InstructorAccessCode;
 use Faker\Generator as Faker;
 
 $factory->define(InstructorAccessCode::class, function (Faker $faker) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $access_code = '';
+
+    for ($i = 0; $i < 10; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $access_code .= $characters[$index];
+    }
     return [
-        //
+        'access_code' => $access_code
     ];
 });
