f310a45129c6be715b0502b6797de68c356c9fdc
Eric Kean <kreut@hotmail.com>
2020-12-21 10:50:27 -0800

    Percent decrease per submission should be less than the learning tree exploration value

--- app/Http/Requests/StoreAssignment.php
+++ app/Http/Requests/StoreAssignment.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 
 use App\Rules\IsValidPeriodOfTime;
+use App\Rules\IsValidSubmissionCountPercentDecrease;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -55,7 +56,8 @@ class StoreAssignment extends FormRequest
         if ($this->assessment_type === 'learning tree') {
             $rules['min_time_needed_in_learning_tree'] = 'required|integer|min:0|max:20';
             $rules['percent_earned_for_exploring_learning_tree'] = 'required|integer|min:0|max:100';
-            $rules['submission_count_percent_decrease'] = 'required|integer|min:0|max:100';
+            $rules['submission_count_percent_decrease'] =  new IsValidSubmissionCountPercentDecrease($this->percent_earned_for_exploring_learning_tree);
+
         }
 
         if ($this->late_policy === 'deduction'){
