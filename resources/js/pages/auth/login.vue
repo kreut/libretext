599815ac587abb974f11fe9b26f05ed0cdb356d9
Eric Kean <kreut@hotmail.com>
2021-01-12 04:37:28 -0800

    Better login form

--- resources/js/pages/auth/login.vue
+++ resources/js/pages/auth/login.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="row pb-5">
     <div class="col-lg-8 m-auto">
-      <card :title="$t('login')">
+      <card v-if="!inIFrame" :title="$t('login')">
         <form @submit.prevent="login" @keydown="form.onKeydown($event)">
           <!-- Email -->
           <div class="form-group row">
             <label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
             <div class="col-md-7">
-              <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email">
+              <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control"
+                     type="email" name="email"
+              >
               <has-error :form="form" field="email" />
             </div>
           </div>
@@ -16,7 +18,9 @@
           <div class="form-group row">
             <label class="col-md-3 col-form-label text-md-right">{{ $t('password') }}</label>
             <div class="col-md-7">
-              <input v-model="form.password" :class="{ 'is-invalid': form.errors.has('password') }" class="form-control" type="password" name="password">
+              <input v-model="form.password" :class="{ 'is-invalid': form.errors.has('password') }" class="form-control"
+                     type="password" name="password"
+              >
               <has-error :form="form" field="password" />
             </div>
           </div>
@@ -48,6 +52,16 @@
           </div>
         </form>
       </card>
+      <b-card v-if="inIFrame">
+        <div class="m-auto">
+          <h5 class="font-italic" style="color:#0060bc">
+            You are not logged in!
+          </h5>
+        </div>
+        <h6>Our SSO supports Google, Microsoft, and your Libretext login.</h6>
+        <hr>
+        <span class="float-right"><login-with-libretexts action="Login" /><br></span>
+      </b-card>
     </div>
   </div>
 </template>
@@ -73,9 +87,16 @@ export default {
       email: '',
       password: ''
     }),
-    remember: false
+    remember: false,
+    inIFrame: false
   }),
-
+  created () {
+    try {
+      this.inIFrame = window.self !== window.top
+    } catch (e) {
+      this.inIFrame = true
+    }
+  },
   methods: {
     async login () {
       // Submit the form.
