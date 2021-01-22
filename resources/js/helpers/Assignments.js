01025f869dc99ee0e55a0fa57bf49e940d32993a
Eric Kean <kreut@hotmail.com>
2021-01-22 12:53:01 -0800

    Fixed locked message

--- resources/js/helpers/Assignments.js
+++ resources/js/helpers/Assignments.js
@@ -19,7 +19,8 @@ export async function getAssignments () {
 export function isLockedMessage () {
   return `This assignment is locked. Since students have submitted responses,
               the only items that you can update are the assignment's name, the assignment's available/due dates,
-              the assignment's group, the instructions, and whether to include the assignment in the final score.`
+              the assignment's group, the instructions, and whether to include the assignment in the final score.  In addition
+              you can still add/remove questions.`
 }
 export function isLocked (assignment) {
   // on the assignments index page I have to pass in the specific assignment
