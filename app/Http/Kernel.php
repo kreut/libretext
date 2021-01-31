266c1aa29efde4df89536a04dec20e06df8fd0f0
Eric Kean <kreut@hotmail.com>
2021-01-31 10:43:02 -0800

    Changed the throttling to 240 per minute

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,7 +44,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'throttle:240,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
