cb0c2d03584bfaa2aa28cc9f387949db981fca61
Eric Kean <kreut@hotmail.com>
2021-02-07 12:24:35 -0800

    Saving titles to the database

--- app/Http/Controllers/LibreverseController.php
+++ app/Http/Controllers/LibreverseController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Title;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
 class LibreverseController extends Controller
@@ -19,8 +21,8 @@ class LibreverseController extends Controller
         $response = Http::get("https://{$library}.libretexts.org/@api/deki/pages/{$pageId}/contents");
 
 
-   $xml = simplexml_load_string($response->body());
- return $xml->body[0];
+        $xml = simplexml_load_string($response->body());
+        return $xml->body[0];
 
     }
 
@@ -31,19 +33,27 @@ class LibreverseController extends Controller
      */
     public function getTitleByLibraryAndPageId(Request $request, string $library, int $pageId)
     {
+        $title_info = DB::table('titles')->where('library', $library)
+            ->where('page_id', $pageId)
+            ->first();
+        if ($title_info) {
+            return $title_info->title;
+        }
+
         $response = Http::get("https://{$library}.libretexts.org/@api/deki/pages/{$pageId}/contents");
-
-
         $xml = simplexml_load_string($response->body());
         if (($pos = strpos($xml->attributes()->title[0], ":")) !== FALSE) {
-            return substr($xml->attributes()->title[0], $pos+1);
+            $title = substr($xml->attributes()->title[0], $pos + 1);
+        } else {
+            $title = $xml->attributes()->title[0];
         }
-        return $xml->attributes()->title[0];
+        Title::create(['library' => $library,
+            'page_id' => $pageId,
+            'title' => $title]);
+        return $title;
 
 
     }
-
-
 
 
 }
