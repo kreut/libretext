a40c2611508cf22cd3668840e4c8b2e5c25a30ef
Eric Kean <kreut@hotmail.com>
2021-03-30 06:19:47 -0700

    Changed the welcome page to add the reduced image

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div class="basic-layout d-flex  justify-content-center bg" :style="{backgroundImage:'url(/assets/img/soundtrap-ddwbTn5HDdQ-unsplash.jpg)'}">
+    <div class="basic-layout d-flex  justify-content-center bg"
+         :style="{backgroundImage:'url(/assets/img/splash.jpg)'}"
+    >
       <div class="text-center">
         <div class="title font-italic text-white">
           {{ title }}
@@ -10,63 +12,64 @@
         </h5>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-  import {mapGetters} from 'vuex'
+import { mapGetters } from 'vuex'
 
-  export default {
+export default {
 
-    metaInfo() {
-      return {title: this.$t('home')}
-    },
+  metaInfo () {
+    return { title: this.$t('home') }
+  },
 
-    data: () => ({
-      title: window.config.appName
-    }),
+  data: () => ({
+    title: window.config.appName
+  }),
 
-    computed: mapGetters({
-      authenticated: 'auth/check'
-    })
-  }
+  computed: mapGetters({
+    authenticated: 'auth/check'
+  })
+}
 </script>
 
 <style lang="scss" scoped>
-  .bg {
-    //background-image: url("~@/assets/img/hatice-yardim-5LvC-QX0OOc-unsplash.jpg");
-    opacity: 0.6;
-    height: 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-  .top-right {
-    position: absolute;
-    right: 10px;
-    top: 18px;
-  }
+.bg {
+  //background-image: url("~@/assets/img/hatice-yardim-5LvC-QX0OOc-unsplash.jpg");
+  opacity: 0.6;
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 
-  .title {
-    font-size: 85px;
-  }
+.top-right {
+  position: absolute;
+  right: 10px;
+  top: 18px;
+}
 
-  .basic-layout {
+.title {
+  font-size: 85px;
+}
+
+.basic-layout {
+  color: #636b6f;
+  height: 100vh;
+  font-weight: 100;
+  position: relative;
+  padding-top: 100px;
+
+  .links > a {
     color: #636b6f;
-    height: 100vh;
-    font-weight: 100;
-    position: relative;
-    padding-top: 100px;
-
-    .links > a {
-      color: #636b6f;
-      padding: 0 25px;
-      font-size: 12px;
-      font-weight: 600;
-      letter-spacing: .1rem;
-      text-decoration: none;
-      text-transform: uppercase;
-    }
+    padding: 0 25px;
+    font-size: 12px;
+    font-weight: 600;
+    letter-spacing: .1rem;
+    text-decoration: none;
+    text-transform: uppercase;
   }
+}
 
 </style>
