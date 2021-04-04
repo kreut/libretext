f083a54ae993c44f9c997d5063da8d89724d34f4
Eric Kean <kreut@hotmail.com>
2021-04-04 03:00:46 -0700

    Put the imathas seed back

--- config/myconfig.php
+++ config/myconfig.php
@@ -3,6 +3,6 @@ return [
     'is_me_cookie' => env('IS_ME_COOKIE', 'no is me cookie'),
     'telegram_channel_id' => env('TELEGRAM_CHANNEL_ID', 'no telegram channel id'),
     'webwork_seed' =>env('WEBWORK_SEED', 'no webwork seed'),
-    'imathas_seed' =>env('IMATHAS_SEED_SEED', 'no imathas seed'),
+    'imathas_seed' =>env('IMATHAS_SEED', 'no imathas seed'),
     'jwt_secret' => env('JWT_SECRET',null)
 ];
