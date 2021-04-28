d3caf224fb3e9498988ba1dff9696aaad84c95f4
Eric Kean <kreut@hotmail.com>
2021-04-28 10:11:06 -0700

    Check to see if JWE/webwork file exists before evaluating

--- config/myconfig.php
+++ config/myconfig.php
@@ -2,10 +2,12 @@
 return [
     'is_me_cookie' => env('IS_ME_COOKIE', 'no is me cookie'),
     'telegram_channel_id' => env('TELEGRAM_CHANNEL_ID', 'no telegram channel id'),
-    'webwork_seed' =>env('WEBWORK_SEED', 'no webwork seed'),
-    'imathas_seed' =>env('IMATHAS_SEED', 'no imathas seed'),
-    'jwt_secret' => env('JWT_SECRET',null),
-    'webwork_jwt_secret' => env('WEBWORK_JWT_SECRET', file_get_contents(base_path() . '/JWE/webwork')),
+    'webwork_seed' => env('WEBWORK_SEED', 'no webwork seed'),
+    'imathas_seed' => env('IMATHAS_SEED', 'no imathas seed'),
+    'jwt_secret' => env('JWT_SECRET', null),
+    'webwork_jwt_secret' => env('WEBWORK_JWT_SECRET', file_exists(base_path() . '/JWE/webwork')
+        ? file_get_contents(base_path() . '/JWE/webwork')
+        : 'No string exists'),
     'db_host' => env('DB_HOST', 'No host provided'),
-    'minpods' => env('MINPODS','None defined')
+    'minpods' => env('MINPODS', 'None defined')
 ];
