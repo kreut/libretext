7dea0b1e80019efa142a62dfff84f0abf4a98a64
Henry Agnew <henryd.agnew@gmail.com>
2021-04-29 22:15:13 -0700

    LoadTest for loop

--- tests/LoadTest/index.js
+++ tests/LoadTest/index.js
@@ -1,31 +1,55 @@
-const jsdom = require('jsdom');
-const async = require('async');
-const puppeteer = require('puppeteer');
-const { JSDOM } = jsdom;
+const jsdom = require('jsdom')
+const async = require('async')
+const puppeteer = require('puppeteer')
+const { JSDOM } = jsdom
 
+const targetPages = [`https://adapt.k8.libretexts.org/`,
+  'https://adapt.k8.libretexts.org/students/courses/45/assignments',//Assignments page:
+  'https://adapt.k8.libretexts.org/students/assignments/392/summary', //Assignment Summary:
+  'https://adapt.k8.libretexts.org/assignments/392/questions/view', //View assessments:
+  'https://adapt.k8.libretexts.org/assignments/392/questions/view/98770', //Page with files from Query:
+  'https://adapt.k8.libretexts.org/assignments/392/questions/view/98760'] //Webwork question:
+const DRIVER = 'JSD'
+// const DRIVER = "pup"
 
-const total = Math.max(0, 1000);
-const concurrency = Math.min(total, 100);
-const targetPage = `https://adapt.k8.libretexts.org/`
+main().then()
 
-JSD().then();
-// pup().then();
-
-async function JSD () {
+async function main () {
+  let total = Math.max(0, 1000)
+  let concurrency
   
-  console.time('JSD');
-  let completed = 0;
-  await async.timesLimit(total, concurrency, async (i)=>{
-    const dom = await JSDOM.fromURL(targetPage, {
-      resources: "usable",
-    });
-    // console.log(i);
-    process.stdout.write(`${++completed}\r`);
-  });
-  console.timeEnd('JSD');
+  for (let page of targetPages) {
+    console.log(page)
+    switch (DRIVER) {
+      case 'JSD':
+        concurrency = Math.min(total, 100)
+        await JSD(page, total, concurrency)
+        break
+      case 'pup':
+        concurrency = Math.min(total, 10)
+        await pup(page, total, concurrency)
+        break
+      default:
+        throw Error(`Invalid Drive ${DRIVER}`)
+    }
+  }
 }
 
-async function pup(){
+async function JSD (targetPage, total, concurrency) {
+  
+  console.time('JSD')
+  let completed = 0
+  await async.timesLimit(total, concurrency, async (i) => {
+    const dom = await JSDOM.fromURL(targetPage, {
+      resources: 'usable',
+    })
+    // console.log(i)
+    process.stdout.write(`${++completed}\r`)
+  })
+  console.timeEnd('JSD')
+}
+
+async function pup (targetPage, total, concurrency) {
   const browser = await puppeteer.launch({
     args: [
       '--no-sandbox',
@@ -33,20 +57,21 @@ async function pup(){
     ],
     // headless: false,
     // devtools: true,
-  });
-  console.time('Puppeteer');
-  let completed = 0;
-  await async.timesLimit(total, concurrency, async (i)=>{
-    const page = await browser.newPage();
+  })
+  console.time('Puppeteer')
+  let completed = 0
+  await async.timesLimit(total, concurrency, async (i) => {
+    const page = await browser.newPage()
     await page.goto(targetPage, {
       timeout: 50000,
-      waitUntil: ["load", "domcontentloaded", 'networkidle0']
-    });
-    // console.log(i);
-    process.stdout.write(`${++completed}\r`);
-  
-    if (!page.isClosed())
-      await page.close();
-  });
-  console.timeEnd('Puppeteer');
+      waitUntil: ['load', 'domcontentloaded', 'networkidle0']
+    })
+    // console.log(i)
+    process.stdout.write(`${++completed}\r`)
+    
+    if (!page.isClosed()) {
+      await page.close()
+    }
+  })
+  console.timeEnd('Puppeteer')
 }
