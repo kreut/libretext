9463b26d7145db94e93ab523619e313d40cbef1d
Eric Kean <kreut@hotmail.com>
2021-07-15 12:17:22 -0700

    Fixed bug with question not showing in get questions

--- resources/js/pages/instructors/questions.get.vue
+++ resources/js/pages/instructors/questions.get.vue
@@ -18,7 +18,7 @@
           :accept="getAcceptedFileTypes()"
         />
         <div v-if="uploading">
-          <b-spinner small type="grow" />
+          <b-spinner small type="grow"/>
           Uploading file...
         </div>
         <input type="hidden" class="form-control is-invalid">
@@ -101,7 +101,7 @@
                background="#FFFFFF"
       />
       <div v-if="!isLoading">
-        <PageTitle :title="title" />
+        <PageTitle :title="title"/>
         <b-container>
           <AssessmentTypeWarnings :assessment-type="assessmentType"
                                   :open-ended-questions-in-real-time="openEndedQuestionsInRealTime"
@@ -128,7 +128,7 @@
                   <template slot="label">
                     School
                     <span id="school_tooltip">
-                      <b-icon class="text-muted" icon="question-circle" /></span>
+                      <b-icon class="text-muted" icon="question-circle"/></span>
                   </template>
                   <b-tooltip target="school_tooltip"
                              delay="250"
@@ -150,7 +150,7 @@
                       />
                     </b-col>
 
-                    <b-col />
+                    <b-col/>
                   </b-form-row>
                 </b-form-group>
                 <b-form-group
@@ -248,16 +248,16 @@
                       </b-col>
                       <b-col class="text-right">
                         <a href="" @click.prevent="addAllQuestions()">
-                          <span class="font-italic"><b-icon icon="plus-circle" /> Add all questions</span>
+                          <span class="font-italic"><b-icon icon="plus-circle"/> Add all questions</span>
                         </a>
                       </b-col>
                     </b-row>
                     <table class="table dragArea table-striped">
                       <thead>
-                        <tr>
-                          <th>Title</th>
-                          <th>Submission</th>
-                        </tr>
+                      <tr>
+                        <th>Title</th>
+                        <th>Submission</th>
+                      </tr>
                       </thead>
                       <draggable v-model="publicCourseAssignmentQuestions"
                                  :group="'remixerQuestions'"
@@ -288,11 +288,11 @@
                     </h5>
                     <table class="table dragArea table-striped">
                       <thead>
-                        <tr>
-                          <th>Order</th>
-                          <th>Title</th>
-                          <th>Submission</th>
-                        </tr>
+                      <tr>
+                        <th>Order</th>
+                        <th>Title</th>
+                        <th>Submission</th>
+                      </tr>
                       </thead>
                       <draggable v-model="chosenPublicCourseAssignmentQuestions"
                                  :options="{group:'remixerQuestions'}"
@@ -311,7 +311,7 @@
                             <a href="" @click.stop.prevent="viewQuestion(question.question_id,'remove')">
                               {{ question.title ? question.title : 'No title' }}
                             </a>
-                            <b-icon icon="trash" @click="removeQuestionFromAssignment(question.question_id)" />
+                            <b-icon icon="trash" @click="removeQuestionFromAssignment(question.question_id)"/>
                           </td>
                           <td class="dragArea">
                             {{ question.submission }}
@@ -356,7 +356,7 @@
                               Add Tag
                             </b-button>
                             <b-button variant="success" size="sm" class="mr-2" @click="getQuestionsByTags()">
-                              <b-spinner v-if="gettingQuestions" small type="grow" />
+                              <b-spinner v-if="gettingQuestions" small type="grow"/>
                               Get Questions
                             </b-button>
                           </div>
@@ -367,7 +367,7 @@
                             <ol>
                               <li v-for="chosenTag in chosenTags" :key="chosenTag">
                                 <span @click="removeTag(chosenTag)">{{ chosenTag }}
-                                  <b-icon icon="trash" variant="danger" /></span>
+                                  <b-icon icon="trash" variant="danger"/></span>
                               </li>
                             </ol>
                           </div>
@@ -432,7 +432,7 @@
                         />
                         <div class="float-right mt-2">
                           <b-button variant="success" size="sm" class="mr-2" @click="directImportQuestions()">
-                            <b-spinner v-if="directImportingQuestions" small type="grow" />
+                            <b-spinner v-if="directImportingQuestions" small type="grow"/>
                             Import Questions
                           </b-button>
                         </div>
@@ -498,7 +498,15 @@
                   style="width: 1px;min-width: 100%;"
           />
         </div>
-        <div v-if="questions[currentPage-1]" v-html="questions[currentPage-1].technology_iframe" />
+        <div v-if="questions[currentPage-1] && questions[currentPage-1].technology_iframe">
+          <iframe
+            :key="`technology-iframe-${questions[currentPage-1].id}`"
+            v-resize="{ log: true, checkOrigin: false }"
+            width="100%"
+            :src="questions[currentPage-1].technology_iframe"
+            frameborder="0"
+          />
+        </div>
       </div>
     </div>
   </div>
