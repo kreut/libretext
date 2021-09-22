d9962c3f635d468b802c640712958b8c9759c9d8
Eric Kean <kreut@hotmail.com>
2021-09-22 07:39:59 -0700

    Updated datashops with the additional information

--- app/Console/Commands/OneTimers/fixDataShopLevelClassAndAddDueDate.php
+++ app/Console/Commands/OneTimers/fixDataShopLevelClassAndAddDueDate.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands\OneTimers;
 
+use App\Question;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -36,59 +37,87 @@ class fixDataShopLevelClassAndAddDueDate extends Command
      *
      * @return int
      */
-    public function handle()
+    public function handle(): int
     {
         $data_shops = DB::table('data_shops')
             ->where('status', 'not fixed')
-            ->limit(30000)
+            ->limit(20000)
             ->get();
 
         foreach ($data_shops as $key => $data_shop) {
-            echo $key . "\r\n";
             $enrolled_courses = DB::table('users')
                 ->join('enrollments', 'users.id', '=', 'enrollments.user_id')
                 ->where('users.email', $data_shop->anon_student_id)
                 ->select('course_id')
                 ->get()
                 ->pluck('course_id');
-            if (!$enrolled_courses) {
-                DB::table('data_shops')->where('id', $data_shop->id)->update(['status'=>"can't fix"]);
-                continue;
+            if ($enrolled_courses->isNotEmpty()) {
+                $assignment_id_exists = DB::table('assignment_question')
+                    ->join('assignments', 'assignment_question.assignment_id', '=', 'assignments.id')
+                    ->where('question_id', $data_shop->problem_name)
+                    ->whereIn('assignments.course_id', $enrolled_courses)
+                    ->select('assignments.id', 'assignments.name', 'assignments.assessment_type')
+                    ->first();
+                $enrolled_message = 'enrolled';
+            } else {
+                $assignment_id_exists = DB::table('assignment_question')
+                    ->where('question_id', $data_shop->problem_name)
+                    ->join('assignments', 'assignment_question.assignment_id', '=', 'assignments.id')
+                    ->select('assignments.id', 'assignments.name', 'assignments.assessment_type')
+                    ->first();
+                $enrolled_message = 'not enrolled';
             }
-            $assignment_id_exists = DB::table('assignment_question')
-                ->join('assignments', 'assignment_question.assignment_id', '=', 'assignments.id')
-                ->where('question_id', $data_shop->problem_name)
-                ->whereIn('assignments.course_id', $enrolled_courses)
-                ->select('assignments.id')
-                ->first();
-
             if ($assignment_id_exists) {
                 $assignment_id = $assignment_id_exists->id;
-                $assignment_info = DB::table('assignments')
-                    ->join('courses', 'assignments.course_id', '=', 'courses.id')
+                $assignment_group_info = DB::table('assignments')
+                    ->join('assignment_groups', 'assignments.assignment_group_id', '=', 'assignment_groups.id')
                     ->where('assignments.id', $assignment_id)
+                    ->select('assignment_groups.assignment_group')
                     ->first();
 
-                $school = $assignment_info->school_id;
-                $course_id = $assignment_info->course_id;
+                $assignment_info = DB::table('assignments')
+                    ->join('courses', 'assignments.course_id', '=', 'courses.id')
+                    ->join('users', 'courses.user_id', '=', 'users.id')
+                    ->where('assignments.id', $assignment_id)
+                    ->select('users.first_name',
+                        'users.last_name',
+                        'courses.name AS course_name',
+                        'courses.start_date as course_start_date',
+                        'users.email',
+                        'courses.school_id',
+                        'courses.id AS course_id')
+                    ->first();
+
+
                 $assign_to_timing = DB::table('assign_to_timings')
                     ->join('assign_to_users', 'assign_to_timings.id', '=', 'assign_to_users.assign_to_timing_id')
                     ->where('assignment_id', $assignment_id)
                     ->first();
+                $question = Question::find($data_shop->problem_name);
 
                 DB::table('data_shops')
                     ->where('id', $data_shop->id)
                     ->update([
-                    'level' => $assignment_id,
-                    'school' => $school,
-                    'class' => $course_id,
-                    'due' => $assign_to_timing->due,
-                    'status' => 'fixed']);
-
+                        'level' => $assignment_id,
+                        'level_name' => $assignment_id_exists->name,
+                        'level_group' => $assignment_group_info->assignment_group ?: null,
+                        'school' => $assignment_info->school_id,
+                        'class' => $assignment_info->course_id,
+                        'library' => $question->library,
+                        'page_id' => $question->page_id,
+                        'class_name' => $assignment_info->course_name,
+                        'class_start_date' => $assignment_info->course_start_date,
+                        'instructor_name' => "$assignment_info->first_name $assignment_info->last_name",
+                        'instructor_email' => $assignment_info->email,
+                        'number_of_attempts_allowed' => $assignment_id_exists->assessment_type === 'delayed' ? 'unlimited' : '1',
+                        'due' => $assign_to_timing ? $assign_to_timing->due : null,
+                        'status' => 'fixed']);
+                $fixed_message = 'fixed';
             } else {
-                DB::table('data_shops')->where('id', $data_shop->id)->update(['status'=>"can't fix"]);
-
+                DB::table('data_shops')->where('id', $data_shop->id)->update(['status' => "can't fix"]);
+                $fixed_message = "can't fix";
             }
+            echo "$key $enrolled_message $fixed_message\r\n";
         }
         return 0;
     }
--- app/DataShop.php
+++ app/DataShop.php
@@ -5,6 +5,7 @@ namespace App;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DataShop extends Model
 {
@@ -16,17 +17,38 @@ class DataShop extends Model
      * @param $data
      * @param $assignment
      */
-   public function store($submission, $data, Assignment $assignment){
-       $this->anon_student_id =  Auth::user()->email ? Auth::user()->email : 'test';
-       $this->session_id = session()->get('submission_id');
-       $this->time = Carbon::now();
-       $this->level = $assignment->id;
-       $this->problem_name = $submission->question_id;
-       $this->problem_view = $submission->submission_count;
-       $this->outcome = $data['all_correct'] ? 'CORRECT' : 'INCORRECT';
-       $this->school = $assignment->course->school_id;
-       $this->class = $assignment->course->id;
-       $this->save();
-   }
+    public function store($submission, $data, Assignment $assignment)
+    {
+        $extra_info = DB::table('assignments')
+            ->join('courses', 'assignments.course_id', '=', 'courses.id')
+            ->join('users', 'courses.user_id', '=', 'users.id')
+            ->join('assignment_groups','assignments.assignment_group_id','=','assignment_groups.id')
+            ->select('users.email', 'users.first_name', 'users.last_name','assignment_groups.assignment_group')
+            ->where('assignments.id',$assignment->id)
+            ->first();
+        $question = Question::find($submission->question_id);
+        $this->anon_student_id = Auth::user()->email ? Auth::user()->email : 'test';
+        $this->session_id = session()->get('submission_id');
+        $this->time = Carbon::now();
+        $this->level = $assignment->id;
+        $this->level_name = $assignment->name;
+        $this->level_group = $extra_info->assignment_group;
+        $this->number_of_attempts_allowed = $assignment->assessment_type === 'delayed' ? 'unlimited' : '1';
+        $this->problem_name = $submission->question_id;
+        $this->library = $question->library;
+        $this->page_id = $question->page_id;
+        $this->problem_view = $submission->submission_count;
+        $this->outcome = $data['all_correct'] ? 'CORRECT' : 'INCORRECT';
+        $this->due = $assignment->assignToTimingByUser('due');
+        $this->school = $assignment->course->school_id;
+        $this->class = $assignment->course->id;
+        $this->class_name = $assignment->course->name;
+        $this->class_start_date = $assignment->course->start_date;
+        $this->instructor_name = "$extra_info->first_name $extra_info->last_name";
+        $this->instructor_email = $extra_info->email;
+        $this->status = 'fixed';
+
+        $this->save();
+    }
 
 }
