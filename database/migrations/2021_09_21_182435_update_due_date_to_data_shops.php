06e6073b0e9a47d6e128675ba711e9398893baf5
Eric Kean <kreut@hotmail.com>
2021-09-22 07:39:59 -0700

    Updated datashops with the additional information

--- database/migrations/2021_09_21_182435_update_due_date_to_data_shops.php
+++ database/migrations/2021_09_21_182435_update_due_date_to_data_shops.php
@@ -15,7 +15,16 @@ class UpdateDueDateToDataShops extends Migration
     {
         Schema::table('data_shops', function (Blueprint $table) {
             $table->dateTime('due')->after('outcome')->nullable();
-            $table->string('status')->after('class')->default('not fixed');
+            $table->string('level_name')->after('level')->nullable();
+            $table->string('level_group')->after('level_name')->nullable();
+            $table->string('library')->after('problem_name')->nullable();
+            $table->string('page_id')->after('library')->nullable();
+            $table->string('number_of_attempts_allowed')->after('level_group')->nullable();
+            $table->string('class_name')->after('class')->nullable();
+            $table->string('class_start_date')->after('class_name')->nullable();
+            $table->string('instructor_name')->after('class_name')->nullable();
+            $table->string('instructor_email')->after('instructor_name')->nullable();
+            $table->string('status')->after('instructor_email')->default('not fixed')->nullable();
         });
     }
 
@@ -27,8 +36,8 @@ class UpdateDueDateToDataShops extends Migration
     public function down()
     {
         Schema::table('data_shops', function (Blueprint $table) {
-            $table->dropColumn('due');
-            $table->dropColumn('fixed');
+            $table->dropColumn(['due','level_name','level_group','library','page_id','number_of_attempts_allowed','class_name','class_start_date','instructor_name','instructor_email','status']);
+
         });
     }
 }
