2e250b8b35d17084aef6c21a88f26d6eaff26002
Eric Kean <kreut@hotmail.com>
2022-01-11 06:36:51 -0800

    Fix for checking if there's at least one local solution implying a downloadable solution

--- app/Solution.php
+++ app/Solution.php
@@ -27,10 +27,19 @@ class Solution extends Model
             ->whereIn('assignment_id', $assignment_ids)
             ->where('user_id', $course->user_id)
             ->get();
+        $assignments_with_at_least_one_local_solution = DB::table('assignment_question')
+            ->join('solutions', 'assignment_question.question_id','=','solutions.question_id')
+            ->whereIn('assignment_question.assignment_id', $assignment_ids)
+            ->where('user_id', $course->user_id)
+            ->groupBy('assignment_question.assignment_id')
+            ->get('assignment_question.assignment_id')
+            ->pluck('assignment_id')
+            ->toArray();
 
         if ($solutions->isNotEmpty()) {
             foreach ($solutions as $key => $value) {
-                if ($solutions_released_by_assignment[$value->assignment_id]) {
+                if ($solutions_released_by_assignment[$value->assignment_id] && in_array($value->assignment_id, $assignments_with_at_least_one_local_solution)) {
+                  //only show the compiled if there's at least one PDF.  Otherwise, they're all local solutions
                     $solutions_by_assignment[$value->assignment_id] = $value->original_filename;
                 }
             }
