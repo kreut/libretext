bc4d08b84d068311101fc4bd273578f87eeafaec
Eric Kean <kreut@hotmail.com>
2022-01-12 07:22:30 -0800

    Updating folder names works

--- tests/Feature/Instructors/SavedFoldersTest.php
+++ tests/Feature/Instructors/SavedFoldersTest.php
@@ -28,6 +28,10 @@ class SavedFoldersTest extends TestCase
      * @var Collection|Model|mixed
      */
     private $student_user;
+    /**
+     * @var string[]
+     */
+    private $saved_question_folder;
 
     /** Should test that only an instructor can create an assignment... */
     public function setup(): void
@@ -35,12 +39,50 @@ class SavedFoldersTest extends TestCase
 
         parent::setUp();
         $this->user = factory(User::class)->create(['role' => 2]);
+        $this->user_2 = factory(User::class)->create(['role' => 2]);
         $this->course = factory(Course::class)->create(['user_id' => $this->user->id]);
         $this->student_user = factory(User::class)->create(['role'=> 3]);
         $this->saved_question_folder = ['type' => 'my_favorites',
             'name' => 'some name'];
 
     }
+
+    /** @test */
+    public function non_folder_owners_cannot_delete_other_folders() {
+        /*$this->actingAs($this->user)->postJson("/api/saved-questions-folders", $this->saved_question_folder)
+            ->assertJson(['type' => 'success']);
+        $saved_question_folder = DB::table('saved_questions_folders')->first();
+        $this->saved_question_folder['folder_id'] = $saved_question_folder->id;
+        /**
+         *
+         */
+    }
+
+    /** @test */
+    public function non_folder_owners_cannot_update_folders()
+    {
+        $this->actingAs($this->user)->postJson("/api/saved-questions-folders", $this->saved_question_folder)
+            ->assertJson(['type' => 'success']);
+        $saved_question_folder = DB::table('saved_questions_folders')->first();
+        $this->saved_question_folder['folder_id'] = $saved_question_folder->id;
+        $this->actingAs($this->user_2)->patchJson("/api/saved-questions-folders", $this->saved_question_folder)
+            ->assertJson(['message' => 'You are not allowed to update this folder.']);
+
+    }
+
+    /** @test */
+    public function folder_owners_can_update_their_own_folders()
+    {
+        $this->actingAs($this->user)->postJson("/api/saved-questions-folders", $this->saved_question_folder)
+            ->assertJson(['type' => 'success']);
+        $saved_question_folder = DB::table('saved_questions_folders')->first();
+        $this->saved_question_folder['folder_id'] = $saved_question_folder->id;
+        $this->saved_question_folder['name'] = "New folder name";
+        $this->actingAs($this->user)->patchJson("/api/saved-questions-folders", $this->saved_question_folder)
+            ->assertJson(['message' => 'The folder some name has been updated.']);
+
+    }
+
 
     /** @test */
     public function folder_names_must_not_be_empty()
@@ -79,13 +121,6 @@ class SavedFoldersTest extends TestCase
 
     }
 
-    /** @test */
-    public function instructors_can_update_folders()
-    {
-        $this->actingAs($this->user)->postJson("/api/saved-questions-folders", $this->saved_question_folder)
-            ->assertJson(['type' => 'success']);
-
-    }
 
 
     /** @test */
