1a20f2c3af4657992ed5970de0f972b77fc950f5
Eric Kean <kreut@hotmail.com>
2022-01-25 06:05:34 -0800

    Accessibility: list on assignment summary page

--- resources/js/pages/students/assignments.summary.vue
+++ resources/js/pages/students/assignments.summary.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <AllFormErrors :all-form-errors="allFormErrors" :modal-id="'modal-form-errors-file-upload'"/>
+    <AllFormErrors :all-form-errors="allFormErrors" :modal-id="'modal-form-errors-file-upload'" />
     <b-modal
       id="modal-completed-assignment"
       ref="modalThumbsUp"
@@ -40,7 +40,7 @@
       title="Submission Not Accepted"
     >
       <b-alert variant="danger" :show="true">
-        <span class="font-weight-bold" style="font-size: large" v-html="errorMessage"/>
+        <span class="font-weight-bold" style="font-size: large" v-html="errorMessage" />
       </b-alert>
     </b-modal>
     <b-modal id="modal-confirm-set-page"
@@ -72,7 +72,7 @@
                background="#FFFFFF"
       />
       <div v-if="!isLoading">
-        <PageTitle :title="name"/>
+        <PageTitle :title="name" />
         <b-container>
           <div v-if="assessmentType !== 'clicker' || pastDue">
             <b-row align-h="end">
@@ -88,36 +88,35 @@
             </b-alert>
           </div>
           <div v-show="isInstructorLoggedInAsStudent">
-            <LoggedInAsStudent :student-name="user.first_name + ' ' + user.last_name"/>
+            <LoggedInAsStudent :student-name="user.first_name + ' ' + user.last_name" />
           </div>
           <b-card v-show="assessmentType !== 'clicker'" header="default"
                   header-html="<h2 class=&quot;h5&quot;>Important Information</h2>"
           >
-
             <b-card-text>
-              <p v-if="public_description" class="mb-2">
-                <span class="font-weight-bold">Description: </span> <span v-html="public_description"/>
-              </p>
-              <p v-if="instructions.length" class="mb-2">
-                <span v-html="instructions"/>
-              </p>
-              <p>
-                <span class="font-weight-bold">Number of Points: </span>
-                <span>This assignment is worth a total of {{
+              <ul style="list-style: none;">
+                <li v-if="public_description" class="mb-2">
+                  <span class="font-weight-bold">Description: </span> <span v-html="public_description" />
+                </li>
+                <li v-if="instructions.length" class="mb-2">
+                  <span v-html="instructions" />
+                </li>
+                <li class="mb-2">
+                  <span class="font-weight-bold">Number of Points: </span>
+                  <span>This assignment is worth a total of {{
                     totalPoints
                   }} point{{ totalPoints !== 1 ? 's' : '' }}.</span>
-              </p>
-              <p>
-                <span class="font-weight-bold">Number of Questions: </span>
-                <span>This assignment has {{
+                </li>
+                <li class="mb-2">
+                  <span class="font-weight-bold">Number of Questions: </span>
+                  <span>This assignment has {{
                     items.length
                   }} question{{ items.length !== 1 ? 's' : '' }}.</span>
-              </p>
-              <div v-show="assessmentType === 'real time'">
-                <p>
+                </li>
+                <li v-show="assessmentType === 'real time'" class="mb-2">
                   <span class="font-weight-bold">Number of Allowed Attempts: </span>
                   {{ numberOfAllowedAttempts }}
-                  <QuestionCircleTooltip :id="'number-of-allowed-attempts-tooltip'"/>
+                  <QuestionCircleTooltip :id="'number-of-allowed-attempts-tooltip'" />
                   <b-tooltip target="number-of-allowed-attempts-tooltip"
                              delay="500"
                              triggers="hover focus"
@@ -125,39 +124,42 @@
                     The number of allowed attempts tells you how many times you can re-submit each question before the
                     due date.
                   </b-tooltip>
-                </p>
-                <p v-show="numberOfAllowedAttempts !== '1' && numberOfAllowedAttemptsPenalty>0">
-                <span class="font-weight-bold">Per Attempt Penalty: </span>
-                {{ numberOfAllowedAttemptsPenalty }}%
-                <QuestionCircleTooltip :id="'per-attempt-penalty-tooltip'"/>
-                <b-tooltip target="per-attempt-penalty-tooltip"
-                           delay="500"
-                           triggers="hover focus"
+                </li>
+                <li
+                  v-show="assessmentType === 'real time' && numberOfAllowedAttempts !== '1' && numberOfAllowedAttemptsPenalty>0"
+                  class="mb-2"
                 >
-                  After your first attempt, a penalty of {{ numberOfAllowedAttemptsPenalty }}% will be applied per
-                  attempt.
-                  As an example, if a question is worth 10 points then on the second attempt,
-                  you will
-                  receive {{ 10 * (1 - parseFloat(numberOfAllowedAttemptsPenalty) / 100) }} points.
-                </b-tooltip>
-                </p>
-              </div>
-              <p>
-                <span class="font-weight-bold">Due Date: </span>
-                <span>This assignment is due {{ formattedDue }}.</span>
-                <span v-if="extension">(You have an extension until {{ extension }}).</span>
-              </p>
-              <p>
-                <span class="font-weight-bold">Late Policy: </span>
-                <span> {{ formattedLatePolicy }}</span>
-              </p>
-              <p v-if="bothFileUploadMode && hasAtLeastOneFileUpload">
-                <span class="font-weight-bold">
-                  Open ended submissions:</span>
-                <span>
-                  Upload a single compiled PDF of the questions and assign pages and/or submit individual submissions on each page.
-                </span>
-              </p>
+                  <span class="font-weight-bold">Per Attempt Penalty: </span>
+                  {{ numberOfAllowedAttemptsPenalty }}%
+                  <QuestionCircleTooltip :id="'per-attempt-penalty-tooltip'" />
+                  <b-tooltip target="per-attempt-penalty-tooltip"
+                             delay="500"
+                             triggers="hover focus"
+                  >
+                    After your first attempt, a penalty of {{ numberOfAllowedAttemptsPenalty }}% will be applied per
+                    attempt.
+                    As an example, if a question is worth 10 points then on the second attempt,
+                    you will
+                    receive {{ 10 * (1 - parseFloat(numberOfAllowedAttemptsPenalty) / 100) }} points.
+                  </b-tooltip>
+                </li>
+                <li class="mb-2">
+                  <span class="font-weight-bold">Due Date: </span>
+                  <span>This assignment is due {{ formattedDue }}.</span>
+                  <span v-if="extension">(You have an extension until {{ extension }}).</span>
+                </li>
+                <li class="mb-2">
+                  <span class="font-weight-bold">Late Policy: </span>
+                  <span> {{ formattedLatePolicy }}</span>
+                </li>
+                <li v-if="bothFileUploadMode && hasAtLeastOneFileUpload" class="mb-2">
+                  <span class="font-weight-bold">
+                    Open ended submissions:</span>
+                  <span>
+                    Upload a single compiled PDF of the questions and assign pages and/or submit individual submissions on each page.
+                  </span>
+                </li>
+              </ul>
             </b-card-text>
           </b-card>
 
@@ -192,17 +194,17 @@
               <ul v-if="files.length && (preSignedURL !== '')">
                 <li v-for="file in files" :key="file.id">
                   <span :class="file.success ? 'text-success font-weight-bold' : ''">{{
-                      file.name
-                    }}</span> -
+                    file.name
+                  }}</span> -
                   <span>{{ formatFileSize(file.size) }} </span>
                   <span v-if="file.size > 10000000">Note: large files may take up to a minute to process.</span>
                   <span v-if="file.error" class="text-danger">Error: {{ file.error }}</span>
                   <span v-else-if="file.active" class="ml-2">
-                    <b-spinner small type="grow"/>
+                    <b-spinner small type="grow" />
                     Uploading File...
                   </span>
                   <span v-if="processingFile">
-                    <b-spinner small type="grow"/>
+                    <b-spinner small type="grow" />
                     Processing file...
                   </span>
                   <b-button v-if="!processingFile && (preSignedURL !== '') && (!$refs.upload || !$refs.upload.active)"
@@ -248,8 +250,8 @@
             >
               <template #cell(question_number)="data">
                 <a href="" @click.stop.prevent="viewQuestion(data.item.question_id)"><span style="font-size:large">&nbsp;{{
-                    data.item.question_number
-                  }}&nbsp;</span></a>
+                  data.item.question_number
+                }}&nbsp;</span></a>
               </template>
               <template v-slot:head(last_question_submission)="data">
                 Last Auto Graded Submission
@@ -285,12 +287,12 @@
                   :class="{ 'table-text-danger': data.item.openEndedSubmissionRequired && !data.item.showThumbsUpForOpenEndedSubmission }"
                 >
                   <span v-if="!data.item.showThumbsUpForOpenEndedSubmission">{{
-                      data.item.last_open_ended_submission
-                    }}</span>
+                    data.item.last_open_ended_submission
+                  }}</span>
                   <span v-if="data.item.showThumbsUpForOpenEndedSubmission">
                     <a :href="data.item.submission_file_url" target="_blank">{{
-                        data.item.last_open_ended_submission
-                      }}</a>
+                      data.item.last_open_ended_submission
+                    }}</a>
                   </span>
                 </span>
                 <font-awesome-icon v-show="data.item.showThumbsUpForOpenEndedSubmission" class="text-success"
@@ -348,7 +350,6 @@
                                  :form="questionSubmissionPageForm" field="page"
                       />
                     </div>
-
                   </b-input-group>
                 </div>
                 <div v-else>
@@ -361,7 +362,7 @@
           <b-card v-if="canViewAssignmentStatistics" class="mb-5" header="default"
                   header-html="<h2 class=&quot;h5&quot;>Statistics</h2>"
           >
-            <AssignmentStatistics/>
+            <AssignmentStatistics />
           </b-card>
         </b-container>
       </div>
@@ -460,7 +461,6 @@ export default {
     this.getFullPdfUrlAtPage = getFullPdfUrlAtPage
   },
   async mounted () {
-
     this.assignmentId = this.$route.params.assignmentId
     await this.getAssignmentSummary()
     await this.getSelectedQuestions(this.assignmentId)
