e7761acace3f30991850c7f286ce5b3a66f42fb5
Eric Kean <kreut@hotmail.com>
2022-02-01 13:24:18 -0800

    Fixed leftover rebase in CreateQuestion

--- resources/js/components/questions/CreateQuestion.vue
+++ resources/js/components/questions/CreateQuestion.vue
@@ -18,14 +18,7 @@
     </div>
 
     <b-modal
-<<<<<<< HEAD
-      id="question-to-view-questions-editor"
-||||||| parent of 243a3246 (Fixed double modal loading issue with My Questions)
-      v-if="createModal"
-      id="question-to-view-questions-editor"
-=======
       :id="modalId"
->>>>>>> 243a3246 (Fixed double modal loading issue with My Questions)
       title="Preview Question"
       size="lg"
       :hide-footer="true"
@@ -551,17 +544,7 @@ export default {
       try {
         const { data } = await this.questionForm.post('/api/questions/preview')
         this.questionToView = data.question
-<<<<<<< HEAD
-        this.$bvModal.show('question-to-view-questions-editor')
-||||||| parent of 243a3246 (Fixed double modal loading issue with My Questions)
-        if (!document.getElementById('question-to-view-questions-editor___BV_modal_content_')) {
-          this.createModal = true
-        } else {
-          this.createModal = false
-        }
-=======
         this.$bvModal.show(this.modalId)
->>>>>>> 243a3246 (Fixed double modal loading issue with My Questions)
         this.$nextTick(() => {
           MathJax.Hub.Queue(['Typeset', MathJax.Hub])
         })
