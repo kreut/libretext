64ec47277568d4a7a46ebdd4fcda04dbe9d63a1c
Eric Kean <kreut@hotmail.com>
2022-05-31 11:44:05 -0700

    Changed the session TTL to match the JWT TTL

--- config/session.php
+++ config/session.php
@@ -31,9 +31,9 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 1440),
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
