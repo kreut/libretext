8e8290f6bcf79a868591db04259a9db9eec232ac
Eric Kean <kreut@hotmail.com>
2022-09-14 13:33:41 -0700

    Got the cookie to work using cache

--- app/Lti13Cookie.php
+++ app/Lti13Cookie.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Cookie;
 use Packback\Lti1p3\Interfaces\ICookie;
 
@@ -9,12 +10,12 @@ class Lti13Cookie implements ICookie
 {
     public function getCookie(string $name): ?string
     {
-        return Cookie::get($name, false);
+        return Cookie::get( $name, false);
     }
 
     public function setCookie(string $name, string $value, $exp = 3600, $options = []): void
     {
-        // By default, make the cookie expire within a minute
-        Cookie::queue($name, $value, $exp / 60);
+        Cache::put($name, $value, 60);
+
     }
 }
