efe7feced03bc62ac85e17bb4de8a5c3932a5556
Eric Kean <kreut@hotmail.com>
2022-11-08 18:43:01 -0800

    Better fix for select choice

--- app/Console/Commands/OneTimers/fixSelectChoice.php
+++ app/Console/Commands/OneTimers/fixSelectChoice.php
@@ -42,39 +42,32 @@ class fixSelectChoice extends Command
     {
         try {
             DB::beginTransaction();
-            $no_correct_responses = Question::where('qti_json', 'LIKE', '%"questionType":"select_choice"%')
-                ->where('qti_json', 'NOT LIKE', '%correctResponse%')
+            $select_choices = Question::where('qti_json', 'LIKE', '%"questionType":"select_choice"%')
                 ->get();
 
-            /* $no_correct_responses = DB::table('questions')
-                 ->join('users','questions.question_editor_user_id','=','users.id')
-                 ->where('qti_json', 'LIKE', '%"questionType":"select_choice"%')
-                 ->where('qti_json', 'NOT LIKE', '%correctResponse%')
-                 ->select('email')
-                 ->groupBy('email')
-                 ->get();*/
 
-            foreach ($no_correct_responses as $no_correct_response) {
+            foreach ($select_choices as $select_choice) {
                 $already_added_to_database = DB::table('qti_json_before_fixing_select_choices')
-                    ->where('question_id', $no_correct_response->question_id)
+                    ->where('question_id', $select_choice->id)
                     ->first();
                 if (!$already_added_to_database) {
                     DB::table('qti_json_before_fixing_select_choices')
                         ->insert([
-                            'question_id'=> $no_correct_response->id,
-                            'qti_json'=> $no_correct_response->qti_json,
+                            'question_id' => $select_choice->id,
+                            'qti_json' => $select_choice->qti_json,
                             'created_at' => now(),
-                            'updated_at'=> now()]);
-                    $qti_json = json_decode($no_correct_response->qti_json, 5);
+                            'updated_at' => now()]);
+                    $qti_json = json_decode($select_choice->qti_json, 5);
                     foreach ($qti_json['inline_choice_interactions'] as $interaction_key => $interaction) {
                         foreach ($interaction as $item_key => $item) {
                             $qti_json['inline_choice_interactions'][$interaction_key][$item_key]['correctResponse'] = $item_key === 0;
                         }
                     }
-                    $no_correct_response->qti_json = json_encode($qti_json);
-                    $no_correct_response->save();
-                    echo  $no_correct_response->id . "\r\n";
+                    $select_choice->qti_json = json_encode($qti_json);
+                    $select_choice->save();
+                    echo $select_choice->id . "\r\n";
                 }
+
             }
             DB::commit();
 
