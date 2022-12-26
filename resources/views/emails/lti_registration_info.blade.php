00be12ce98c30e5118eda3e485a9c80bde920cf5
Eric Kean <kreut@hotmail.com>
2022-12-26 07:09:25 -0800

    Added clarifying language to Canvas

--- resources/views/emails/lti_registration_info.blade.php
+++ resources/views/emails/lti_registration_info.blade.php
@@ -11,7 +11,8 @@
   <p>Hi,</p>
   <p>
     Thank you for registering your Canvas LTI information with ADAPT. At this point, you'll need to <a href="https://www.youtube.com/watch?v=0rhJ7CfiF04">add ADAPT as an
-      external app</a>.
+      external app</a>.  One important note about this process: 23 seconds into the video you're asked to "Add App".  If your Canvas installation doesn't have a Configuration Type of
+    LTI 1.3, then instead, please choose "By Client ID" and use Canvas' "Developer Key ID" which you entered into our Canvas Configuration form.
   </p>
   <p>Within ADAPT, instructors should navigate to a particular course that they would like to use within Canvas.  Then, under <a href="https://youtu.be/watch?v=CjvAzBTRM9o">Course Properties</a> for that course
     they should choose "Yes" for LMS to enable the LTI grade passback feature.
