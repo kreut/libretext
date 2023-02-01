a50b56474129fc66fd02a3435b5955caf5655c93
Eric Kean <kreut@hotmail.com>
2023-02-01 06:02:25 -0800

    Firebase implementation

--- app/Services/FCMService.php
+++ app/Services/FCMService.php
@@ -8,7 +8,7 @@ class FCMService
 {
     public static function send($token, $notification)
     {
-        Http::acceptJson()->withToken(config('fcm.token'))->post(
+       return  Http::acceptJson()->withToken(config('fcm.token'))->post(
             'https://fcm.googleapis.com/fcm/send',
             [
                 'to' => $token,
