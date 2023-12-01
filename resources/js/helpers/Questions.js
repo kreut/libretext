4b6fdf2efe97845515625b3fb7a59641aa44ae4a
Eric Kean <kreut@hotmail.com>
2023-12-01 14:30:44 -0800

    Expanded the video player

--- resources/js/helpers/Questions.js
+++ resources/js/helpers/Questions.js
@@ -5,7 +5,7 @@ export function formatQuestionMediaPlayer (htmlString) {
   const currentDomain = window.location.origin
   const regex = new RegExp(`<a\\s+href="${currentDomain}/question-media-player/([^"]+)">([^<]+)<\\/a>`, 'g')
   return htmlString.replace(regex, (match, url) => {
-    return `<iframe class="question-media-player" frameborder="0" src="${currentDomain}/question-media-player/${url}"></iframe></div>`
+    return `<iframe class="question-media-player" style="width: 1px;min-width: 100%;" frameborder="0" src="${currentDomain}/question-media-player/${url}"></iframe></div>`
   })
 }
 
