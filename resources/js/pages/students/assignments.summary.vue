2cfc49337b0679866879155587e3713ef37a5d47
Eric Kean <kreut@hotmail.com>
2024-01-12 05:52:28 -0800

    Missing assignment ID in data

--- resources/js/pages/students/assignments.summary.vue
+++ resources/js/pages/students/assignments.summary.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <AllFormErrors :all-form-errors="allFormErrors" :modal-id="'modal-form-errors-file-upload'"/>
+    <AllFormErrors :all-form-errors="allFormErrors" :modal-id="'modal-form-errors-file-upload'" />
     <b-modal id="modal-last-open-ended-submission"
              title="Last Open-Ended Submission"
              hide-footer
@@ -40,7 +40,7 @@
     >
       <b-alert variant="info" show>
         <span style="font-size: large">
-              All responses successfully submitted.
+          All responses successfully submitted.
         </span>
       </b-alert>
       <b-container>
@@ -72,7 +72,7 @@
       title="Submission Not Accepted"
     >
       <b-alert variant="danger" :show="true">
-        <span class="font-weight-bold" style="font-size: large" v-html="errorMessage"/>
+        <span class="font-weight-bold" style="font-size: large" v-html="errorMessage" />
       </b-alert>
     </b-modal>
     <b-modal id="modal-confirm-set-page"
@@ -104,7 +104,7 @@
                background="#FFFFFF"
       />
       <div v-if="!isLoading">
-        <PageTitle :title="name"/>
+        <PageTitle :title="name" />
         <b-container>
           <div v-if="assessmentType !== 'clicker' || pastDue">
             <b-row align-h="end">
@@ -115,10 +115,10 @@
             <hr>
           </div>
           <div v-if="assessmentType === 'clicker' && !pastDue">
-           Please wait for your instructor to open up this assignment.
+            Please wait for your instructor to open up this assignment.
           </div>
           <div v-show="isInstructorLoggedInAsStudent">
-            <LoggedInAsStudent :student-name="user.first_name + ' ' + user.last_name"/>
+            <LoggedInAsStudent :student-name="user.first_name + ' ' + user.last_name" />
           </div>
           <b-card v-show="assessmentType !== 'clicker'" header="default"
                   header-html="<h2 class=&quot;h5&quot;>Important Information</h2>"
@@ -126,22 +126,22 @@
             <b-card-text>
               <ul style="list-style: none;">
                 <li v-if="public_description" class="mb-2">
-                  <span class="font-weight-bold">Description: </span> <span v-html="public_description"/>
+                  <span class="font-weight-bold">Description: </span> <span v-html="public_description" />
                 </li>
                 <li v-if="instructions.length" class="mb-2">
-                  <span v-html="instructions"/>
+                  <span v-html="instructions" />
                 </li>
                 <li class="mb-2">
                   <span class="font-weight-bold">Number of Points: </span>
                   <span>This assignment is worth a total of {{
-                      totalPoints
-                    }} point{{ totalPoints !== 1 ? 's' : '' }}.</span>
+                    totalPoints
+                  }} point{{ totalPoints !== 1 ? 's' : '' }}.</span>
                 </li>
                 <li class="mb-2">
                   <span class="font-weight-bold">Number of Questions: </span>
                   <span>This assignment has {{
-                      items.length
-                    }} question{{ items.length !== 1 ? 's' : '' }}.</span>
+                    items.length
+                  }} question{{ items.length !== 1 ? 's' : '' }}.</span>
                 </li>
                 <li v-show="assessmentType === 'real time'" class="mb-2">
                   <span class="font-weight-bold">Number of Allowed Attempts: </span>
@@ -149,7 +149,6 @@
                   <QuestionCircleTooltipModal :aria-label="'Number of Allowed Attempts'"
                                               :modal-id="'modal-number-of-allowed-attempts'"
                   />
-
                 </li>
                 <li
                   v-show="assessmentType === 'real time' && numberOfAllowedAttempts !== '1' && numberOfAllowedAttemptsPenalty>0"
@@ -160,7 +159,6 @@
                   <QuestionCircleTooltipModal :aria-label="'Per Attempt Penalty'"
                                               :modal-id="'modal-per-attempt-penalty'"
                   />
-
                 </li>
                 <li class="mb-2">
                   <span class="font-weight-bold">Due Date: </span>
@@ -207,17 +205,17 @@
               <ul v-if="files.length && (preSignedURL !== '')">
                 <li v-for="file in files" :key="file.id">
                   <span :class="file.success ? 'text-success font-weight-bold' : ''">{{
-                      file.name
-                    }}</span> -
+                    file.name
+                  }}</span> -
                   <span>{{ formatFileSize(file.size) }} </span>
                   <span v-if="file.size > 10000000">Note: large files may take up to a minute to process.</span>
                   <span v-if="file.error" class="text-danger">Error: {{ file.error }}</span>
                   <span v-else-if="file.active" class="ml-2">
-                    <b-spinner small type="grow"/>
+                    <b-spinner small type="grow" />
                     Uploading File...
                   </span>
                   <span v-if="processingFile">
-                    <b-spinner small type="grow"/>
+                    <b-spinner small type="grow" />
                     Processing file...
                   </span>
                   <b-button v-if="!processingFile && (preSignedURL !== '') && (!$refs.upload || !$refs.upload.active)"
@@ -268,8 +266,8 @@
             >
               <template #cell(question_number)="data">
                 <a href="" @click.stop.prevent="viewQuestion(data.item.question_id)"><span style="font-size:large">&nbsp;{{
-                    data.item.question_number
-                  }}&nbsp;</span></a>
+                  data.item.question_number
+                }}&nbsp;</span></a>
               </template>
               <template v-slot:head(last_question_submission)="data">
                 Last Auto-Graded Submission
@@ -293,12 +291,12 @@
                   :class="{ 'table-text-danger': data.item.openEndedSubmissionRequired && !data.item.showThumbsUpForOpenEndedSubmission }"
                 >
                   <span v-if="!data.item.showThumbsUpForOpenEndedSubmission">{{
-                      data.item.last_open_ended_submission
-                    }}</span>
+                    data.item.last_open_ended_submission
+                  }}</span>
                   <span v-if="data.item.showThumbsUpForOpenEndedSubmission">
                     <a :href="data.item.submission_file_url" target="_blank">{{
-                        data.item.last_open_ended_submission
-                      }}</a>
+                      data.item.last_open_ended_submission
+                    }}</a>
                   </span>
                 </span>
                 <font-awesome-icon v-show="data.item.showThumbsUpForOpenEndedSubmission" class="text-success"
@@ -357,7 +355,7 @@
           <b-card v-if="canViewAssignmentStatistics" class="mb-5" header="default"
                   header-html="<h2 class=&quot;h5&quot;>Statistics</h2>"
           >
-            <AssignmentStatistics/>
+            <AssignmentStatistics />
           </b-card>
         </b-container>
       </div>
@@ -402,6 +400,7 @@ export default {
   },
   middleware: 'auth',
   data: () => ({
+    assignmentId: 0,
     setAllPages: false,
     cacheKey: 0,
     numberOfAllowedAttempts: '',
