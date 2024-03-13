904a03f89e800bcd44ab5caf17b8b70cc7aa0e47
Yasin Dahi <ymdahi@gmail.com>
2024-03-12 22:06:06 -0400

    initial welcome page

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -1,6 +1,71 @@
 <template>
   <div>
-    <div class="text-center">
+    <b-container class="mb-5">
+      <b-row>
+        <b-col>
+          <b-img src="/assets/img/adapt-welcome-1.jpg" fluid alt="Fluid image"></b-img>
+        </b-col>
+        <b-col class="align-self-center">
+          <h1>Welcome to ADAPT</h1>
+          <p class="lead">ADAPT offer the best of pre-built, publisher-quality STEM course content and open educational
+            resource (OER) models coupled with boundless customization possibilities so that your content is the perfect
+            fit for your curriculum.</p>
+          <b-button size="lg" variant="info" @click="$router.push({ path: '/open-courses/commons' })">
+            Browse Open Course
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container class="mt-5">
+      <b-row>
+        <b-col>
+          <div>
+            <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+              <b-row no-gutters>
+                <b-col md="3">
+                  <b-card-img src="/assets/img/adapt-student-welcome.jpg" alt="Image" class="rounded-0"></b-card-img>
+                </b-col>
+                <b-col md="9">
+                  <b-card-body title="Students">
+                    <b-card-text>
+                      Create your Student account to enroll your course.
+                    </b-card-text>
+                    <b-button variant="dark" @click="$router.push({ path: '/open-courses/commons' })">
+                      Create your Student Account
+                    </b-button>
+                  </b-card-body>
+                </b-col>
+              </b-row>
+            </b-card>
+          </div>
+        </b-col>
+        <div class="spacer" style="height: 50px;"></div>
+        <b-col>
+          <div>
+            <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+              <b-row no-gutters>
+                <b-col md="3">
+                  <b-card-img src="/assets/img/adapt-student-welcome.jpg" alt="Image" class="rounded-0"></b-card-img>
+                </b-col>
+                <b-col md="9">
+                  <b-card-body title="Instructors">
+                    <b-card-text>
+                      Create your Instructor account to enroll your course.
+                    </b-card-text>
+                    <b-button variant="dark" @click="$router.push({ path: '/open-courses/commons' })">
+                      Create your Instructor Account
+                    </b-button>
+                  </b-card-body>
+                </b-col>
+              </b-row>
+            </b-card>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <!-- <div class="text-center">
       <h1 class="title text-primary" style="margin-top:100px">
         {{ title }}
       </h1>
@@ -10,7 +75,7 @@
       <b-button size="lg" variant="info" @click="$router.push({path:'/open-courses/commons'})">
         Commons
       </b-button>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -19,7 +84,7 @@ import { mapGetters } from 'vuex'
 
 export default {
 
-  metaInfo () {
+  metaInfo() {
     return { title: 'Home' }
   },
 
@@ -31,18 +96,18 @@ export default {
     authenticated: 'auth/check',
     user: 'auth/user'
   }),
-  mounted () {
+  mounted() {
     if (this.authenticated && this.user.is_tester_student) {
       this.$router.push({ name: 'cannot.view.as.testing.student' })
     }
     this.resizeHandler()
     window.addEventListener('resize', this.resizeHandler)
   },
-  beforeDestroy () {
+  beforeDestroy() {
     window.removeEventListener('resize', this.resizeHandler)
   },
   methods: {
-    resizeHandler () {
+    resizeHandler() {
       let basicLayout = document.getElementsByClassName('basic-layout')
       if (basicLayout.length) {
         this.zoomGreaterThan(1.5)
@@ -81,7 +146,7 @@ export default {
   position: relative;
   padding-top: 100px;
 
-  .links > a {
+  .links>a {
     color: #636b6f;
     padding: 0 25px;
     font-size: 12px;
@@ -91,5 +156,4 @@ export default {
     text-transform: uppercase;
   }
 }
-
 </style>
