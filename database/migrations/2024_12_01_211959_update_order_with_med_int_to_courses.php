2f62ae06cab109836a449187244d2622086debf2
Eric Kean <kreut@hotmail.com>
2024-12-01 13:26:18 -0800

    Changed to medium int

---
+++ database/migrations/2024_12_01_211959_update_order_with_med_int_to_courses.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateOrderWithMedIntToCourses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            DB::statement('ALTER TABLE courses MODIFY COLUMN `order` MEDIUMINT UNSIGNED NOT NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            DB::statement('ALTER TABLE courses MODIFY COLUMN `order` TINYINT UNSIGNED NOT NULL');
+        });
+    }
+}
