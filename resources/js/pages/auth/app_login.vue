e6cbe88460078d6f57fe09fcf4e85dd5fa0024b1
Eric Kean <kreut@hotmail.com>
2025-07-22 05:53:02 -0700

    Fixed initiate login message

--- resources/js/pages/auth/app_login.vue
+++ resources/js/pages/auth/app_login.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
-    <a id="initiate-login" href="/api/oidc/initiate-login/app">initiate app login</a>
+    <div class="vld-parent">
+      <loading :active.sync="isLoading"
+               :can-cancel="true"
+               :is-full-page="true"
+               :width="128"
+               :height="128"
+               color="#007BFF"
+               background="#FFFFFF"
+      />
+      <a v-show="false" id="initiate-login" href="/api/oidc/initiate-login/app">initiate app login</a>
+    </div>
   </div>
 </template>
 
 <script>
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+
 export default {
   name: 'AppLogin',
+  layout: 'blank',
+  components: { Loading },
+  data: () => ({
+    isLoading: true
+  }),
   mounted () {
     document.getElementById('initiate-login').click()
   }
 }
 </script>
-
-<style scoped>
-
-</style>
